2d77e96d7b4114c00b83e22b506f85608100ae4b
jack <461897266@qq.com>
2020-06-03 16:46:52 +0800

    fix header bug

--- lib/screens/chat/home.dart
+++ lib/screens/chat/home.dart
@@ -2,8 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_easyrefresh/bezier_bounce_footer.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:nmobile/blocs/client/client_bloc.dart';
 import 'package:nmobile/blocs/client/client_state.dart';
 import 'package:nmobile/components/button.dart';
