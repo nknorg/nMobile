46f94d5e990fa58b50327b5cecceefc0a3f993f9
chenai <chenai.nakam@gmail.com>
2020-06-10 20:44:13 +0800

    add download progress.

--- lib/components/dialog/apk_upgrade_notes.dart
+++ lib/components/dialog/apk_upgrade_notes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nmobile/blocs/download_progress.dart';
 import 'package:nmobile/components/button.dart';
 import 'package:nmobile/components/markdown.dart';
 import 'package:nmobile/consts/colors.dart';
@@ -14,6 +16,8 @@ typedef OnIgnore = void Function(String version);
 class ApkUpgradeNotesDialog extends StatefulWidget {
   @override
   _ApkUpgradeNotesDialogState createState() => _ApkUpgradeNotesDialogState();
+
+  final DownloadProgressBloc _downloadProgBloc = DownloadProgressBloc();
   final BuildContext _context;
   String _version;
   String _title;
@@ -26,7 +30,8 @@ class ApkUpgradeNotesDialog extends StatefulWidget {
 
   ApkUpgradeNotesDialog.of(this._context);
 
-  show(String version, String title, String notes, bool force, Map jsonMap, OnDownload onDownload, OnIgnore onIgnore, VoidCallback onClose) {
+  DownloadProgressBloc show(
+      String version, String title, String notes, bool force, Map jsonMap, OnDownload onDownload, OnIgnore onIgnore, VoidCallback onClose) {
     this._title = title;
     this._version = version;
     this._notes = notes;
@@ -35,7 +40,7 @@ class ApkUpgradeNotesDialog extends StatefulWidget {
     this._onDownload = onDownload;
     this._onIgnore = onIgnore;
     this._onClose = onClose;
-    return showDialog(
+    showDialog(
       context: _context,
       barrierDismissible: false,
       builder: (ctx) {
@@ -45,6 +50,7 @@ class ApkUpgradeNotesDialog extends StatefulWidget {
         );
       },
     );
+    return _downloadProgBloc;
   }
 
   close() {
@@ -54,6 +60,7 @@ class ApkUpgradeNotesDialog extends StatefulWidget {
 
 class _ApkUpgradeNotesDialogState extends State<ApkUpgradeNotesDialog> {
   double _width;
+  bool _showProgress = false;
 
   @override
   void dispose() {
@@ -63,97 +70,134 @@ class _ApkUpgradeNotesDialogState extends State<ApkUpgradeNotesDialog> {
   @override
   Widget build(BuildContext context) {
     _width = MediaQuery.of(context).size.width - 20;
-    return Material(
-      borderRadius: BorderRadius.all(Radius.circular(20)),
-      color: DefaultTheme.backgroundLightColor,
-      child: Container(
-        width: _width,
-        height: _width,
-        constraints: BoxConstraints(minHeight: 200),
-        child: Flex(
-          direction: Axis.vertical,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
+    return BlocProvider<DownloadProgressBloc>(
+      create: (context) => widget._downloadProgBloc,
+      child: Material(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: DefaultTheme.backgroundLightColor,
+        child: Container(
+          width: _width,
+          height: _width,
+          constraints: BoxConstraints(minHeight: 200),
+          child: Flex(
+            direction: Axis.vertical,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                  flex: 0,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: Button(
+                          icon: true,
+                          padding: 0.pad().pad(t: 6, r: 6),
+                          size: 48,
+                          child: loadAssetIconsImage('close', width: 16),
+                          onPressed: () {
+                            widget.close();
+                            widget._onClose();
+                          }).sized(h: 48).toList)),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget._title != null ? widget._title : NMobileLocalizations.of(context).release_notes,
+                      style: TextStyle(fontSize: DefaultTheme.h2FontSize, color: Colours.dark_2d, fontWeight: FontWeight.bold),
+                      maxLines: 2,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: Markdown(
+                          data: widget._notes,
+                          dark: false, // TextStyle(fontSize: DefaultTheme.h3FontSize, color: Colours.gray_81),
+                        ).toList,
+                      ).pad(t: 12, b: 12),
+                    )
+                  ],
+                ).pad(l: 24, r: 24),
+              ),
+              Expanded(
                 flex: 0,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: Button(
-                        icon: true,
-                        padding: 0.pad().pad(t: 6, r: 6),
-                        size: 48,
-                        child: loadAssetIconsImage('close', width: 16),
-                        onPressed: () {
-                          widget.close();
-                          widget._onClose();
-                        }).sized(h: 48).toList)),
-            Expanded(
-              flex: 1,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    widget._title != null ? widget._title : NMobileLocalizations.of(context).release_notes,
-                    style: TextStyle(fontSize: DefaultTheme.h2FontSize, color: Colours.dark_2d, fontWeight: FontWeight.bold),
-                    maxLines: 2,
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: ListView(
-                      shrinkWrap: true,
-                      children: Markdown(
-                        data: widget._notes,
-                        dark: false, // TextStyle(fontSize: DefaultTheme.h3FontSize, color: Colours.gray_81),
-                      ).toList,
-                    ).pad(t: 12, b: 12),
-                  )
-                ],
-              ).pad(l: 24, r: 24),
-            ),
-            Expanded(
-              flex: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  widget._force
-                      ? Space.empty
-                      : Expanded(
-                          flex: 0,
-                          child: FlatButton(
-                            color: Colors.transparent,
-                            textColor: Colours.pink_f8,
-                            child: Text(
-                              NMobileLocalizations.of(context).ignore,
-                              style: TextStyle(fontStyle: FontStyle.italic, decoration: TextDecoration.underline, decorationColor: Colours.pink_f8),
-                            ),
-                            padding: 0.pad(l: 24, r: 24),
-                            onPressed: () {
-                              widget.close();
-                              widget._onIgnore(widget._version);
-                            },
-                            shape: StadiumBorder(side: BorderSide(style: BorderStyle.none)),
-                          ).sized(h: 48).pad(r: 12),
-                        ),
-                  Expanded(
-                    flex: 1,
-                    child: FlatButton(
-                            color: Colours.blue_0f,
-                            textColor: Colours.white,
-                            child: Text(NMobileLocalizations.of(context).download_now),
-                            padding: 0.pad(l: 24, r: 24),
-                            onPressed: () {
-                              widget.close();
-                              widget._onDownload(widget._jsonMap);
-                            },
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(48)))
-                        .sized(h: 48),
-                  )
-                ],
-              ).pad(l: 24, r: 24, b: 36),
-            ),
-          ],
+                child: BlocBuilder<DownloadProgressBloc, DownloadState>(
+                  builder: (context, state) {
+                    return (state.progress == null && !_showProgress) ? _buildButton(context) : _buildProgress(context, state);
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget _buildProgress(BuildContext context, DownloadState state) {
+    print('download apk: ${state.progress}%');
+    if (state.progress != null && state.progress >= 1.0) {
+      // Exception caught.
+      // widget.close();
+    }
+    return Column(
+      children: <Widget>[
+        LinearProgressIndicator(
+          backgroundColor: Colours.gray_81,
+          valueColor: AlwaysStoppedAnimation(Colours.blue_0f),
+          value: state.progress,
+        ),
+        state.progress == null
+            ? Space.empty
+            : Text(
+                (state.progress * 100).toStringAsFixed(0) + '%',
+                style: TextStyle(color: Colours.blue_0f, fontSize: DefaultTheme.bodySmallFontSize),
+              ).pad(t: 3),
+      ],
+    ).pad(l: 24, t: 12, r: 24, b: (state.progress == null ? 36 : 28));
+  }
+
+  Widget _buildButton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        widget._force
+            ? Space.empty
+            : Expanded(
+                flex: 0,
+                child: FlatButton(
+                  color: Colors.transparent,
+                  textColor: Colours.pink_f8,
+                  child: Text(
+                    NMobileLocalizations.of(context).ignore,
+                    style: TextStyle(fontStyle: FontStyle.italic, decoration: TextDecoration.underline, decorationColor: Colours.pink_f8),
+                  ),
+                  padding: 0.pad(l: 24, r: 24),
+                  onPressed: () {
+                    widget.close();
+                    widget._onIgnore(widget._version);
+                  },
+                  shape: StadiumBorder(side: BorderSide(style: BorderStyle.none)),
+                ).sized(h: 48).pad(r: 12),
+              ),
+        Expanded(
+          flex: 1,
+          child: FlatButton(
+                  color: Colours.blue_0f,
+                  textColor: Colours.white,
+                  child: Text(NMobileLocalizations.of(context).download_now),
+                  padding: 0.pad(l: 24, r: 24),
+                  onPressed: () {
+                    // widget.close();
+                    widget._onDownload(widget._jsonMap);
+                    setState(() {
+                      _showProgress = true;
+                    });
+                  },
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(48)))
+              .sized(h: 48),
+        )
+      ],
+    ).pad(l: 24, r: 24, b: 36);
   }
 }
