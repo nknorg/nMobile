8e053d7abb0df85e12f17e78f76bf52ce04b363d
heron <peng.liu@nkn.org>
2020-06-22 12:16:52 +0800

    fix input number validate

--- lib/screens/wallet/send_nkn.dart
+++ lib/screens/wallet/send_nkn.dart
@@ -289,7 +289,7 @@ class _SendNknScreenState extends State<SendNknScreen> {
                                                               ),
                                                               textInputAction: TextInputAction.next,
                                                               keyboardType: TextInputType.numberWithOptions(decimal: true),
-                                                              inputFormatters: [WhitelistingTextInputFormatter(RegExp('^[0-9]+\\.?[0-9]{0,8}'))],
+                                                              inputFormatters: [WhitelistingTextInputFormatter(RegExp(r'^[0-9]+\.?[0-9]{0,8}'))],
                                                             ),
                                                             Padding(
                                                               padding: const EdgeInsets.only(bottom: 20),
@@ -425,7 +425,7 @@ class _SendNknScreenState extends State<SendNknScreen> {
                                                                         decimal: true,
                                                                       ),
                                                                       textInputAction: TextInputAction.done,
-                                                                      inputFormatters: [WhitelistingTextInputFormatter(RegExp('^[0-9]+\\.?[0-9]{0,8}'))],
+                                                                      inputFormatters: [WhitelistingTextInputFormatter(RegExp(r'^[0-9]+\.?[0-9]{0,8}'))],
                                                                     ),
                                                                   ),
                                                                 ],
