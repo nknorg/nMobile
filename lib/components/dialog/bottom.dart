01ef0ba83f2269343a4a69b1f3e0575c5408b2ed
jack <461897266@qq.com>
2020-07-10 18:40:58 +0800

    update popular group lists 115

--- lib/components/dialog/bottom.dart
+++ lib/components/dialog/bottom.dart
@@ -212,7 +212,7 @@ class BottomDialog extends StatefulWidget {
                     padding: const EdgeInsets.only(top: 24, bottom: 24),
                     child: Label(
                       title,
-                      type: LabelType.h2,
+                      type: LabelType.h3,
                     ),
                   ),
                 ),
@@ -223,7 +223,8 @@ class BottomDialog extends StatefulWidget {
                     children: <Widget>[
                       Label(
                         NMobileLocalizations.of(context).send_to,
-                        type: LabelType.h4,
+                        type: LabelType.bodyRegular,
+                        color: DefaultTheme.fontColor1,
                         textAlign: TextAlign.start,
                       ),
                       Textbox(
