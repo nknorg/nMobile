0363951494fb77f97ae767f4d2a3982dfeb68cf8
heron <peng.liu@nkn.org>
2020-08-05 15:21:19 +0800

    clear code
    Signed-off-by: heron <peng.liu@nkn.org>

--- lib/utils/crach_utils.dart
+++
@@ -1,7 +0,0 @@
-import 'package:catcher/catcher.dart';
-
-CatcherOptions debugOptions = CatcherOptions(PageReportMode(), [ConsoleHandler()]);
-
-CatcherOptions releaseOptions = CatcherOptions(SilentReportMode(), [
-  EmailAutoHandler('smtp.qq.com', 587, '632987138@qq.com', 'NKN Catcher', 'sdepcgebzkpzbdaf', ['461897266@qq.com'], emailTitle: 'NKN Catcher'),
-]);
