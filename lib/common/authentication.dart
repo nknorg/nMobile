7f31800b4c41857b2d0343c9ea11e2387e66b498
heron <peng.liu@nkn.org>
2021-04-28 15:58:09 +0800

    add authentication, chat module
    Signed-off-by: heron <peng.liu@nkn.org>

---
+++ lib/common/authentication.dart
@@ -0,0 +1,28 @@
+import 'package:local_auth/auth_strings.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:nmobile/utils/logger.dart';
+
+
+/// Biometric authentication (Face ID, Touch ID or lock code), pin coode, wallet password
+class Authorization {
+  /// This class provides means to perform local, on-device authentication of the user.
+  /// This means referring to biometric authentication on iOS (Touch ID or lock code) and the fingerprint
+  /// APIs on Android (introduced in Android 6.0).
+  /// doc: https://github.com/flutter/plugins/tree/master/packages/local_auth
+  LocalAuthentication _localAuth = LocalAuthentication();
+
+  Future<bool> get canCheckBiometrics async => await _localAuth.canCheckBiometrics;
+
+  Future<List<BiometricType>> get availableBiometrics async => await _localAuth.getAvailableBiometrics();
+
+  Future<bool> authentication(String localizedReason) async {
+    try {
+      bool success = await _localAuth.authenticate(localizedReason: localizedReason);
+      return success;
+    } catch (e) {
+      logger.e(e);
+    }
+
+    return false;
+  }
+}
--- lib/screens/chat/home.dart
+++ lib/screens/chat/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nmobile/common/locator.dart';
+import 'package:nmobile/components/button/button.dart';
 import 'package:nmobile/components/label.dart';
 import 'package:nmobile/components/layout/header.dart';
 import 'package:nmobile/components/layout/layout.dart';
