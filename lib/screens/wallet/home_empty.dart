2b4385150d8981f32afb1599cdc205f47b27af08
guo <jiangzhiguo1992@163.com>
2021-04-29 16:55:01 +0800

    refactor layout which wallet empty page

--- lib/screens/wallet/home_empty.dart
+++ lib/screens/wallet/home_empty.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/components/button/button.dart';
 import 'package:nmobile/components/label.dart';
-import 'package:nmobile/components/layout/header.dart';
-import 'package:nmobile/components/layout/layout.dart';
 import 'package:nmobile/generated/l10n.dart';
 import 'package:nmobile/screens/wallet/create_nkn.dart';
+import 'package:nmobile/screens/wallet/import_nkn.dart';
 import 'package:nmobile/utils/assets.dart';
 
 class WalletHomeEmptyScreen extends StatefulWidget {
@@ -18,74 +17,65 @@ class _WalletHomeEmptyScreenState extends State<WalletHomeEmptyScreen> {
   @override
   Widget build(BuildContext context) {
     S _localizations = S.of(context);
-    return Layout(
-      headerColor: application.theme.primaryColor,
-      header: Header(
-        titleChild: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Label(
-            _localizations.menu_wallet,
-            type: LabelType.h2,
-            color: application.theme.fontLightColor,
+    return Container(
+      color: application.theme.backgroundColor4,
+      padding: EdgeInsets.fromLTRB(20, 32, 20, 86),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: assetImage("wallet/pig.png", width: MediaQuery.of(context).size.width / 3),
           ),
-        ),
-      ),
-      child: Container(
-        color: application.theme.backgroundColor,
-        constraints: BoxConstraints.expand(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Expanded(
-              flex: 0,
-              child: Container(
-                // color: Colors.amberAccent,
-                child: Padding(
-                  padding: EdgeInsets.only(),
-                  child: assetImage("wallet/pig.png", width: 120),
+          Expanded(
+            flex: 0,
+            child: Column(
+              children: <Widget>[
+                Label(
+                  _localizations.no_wallet_title,
+                  color: application.theme.fontLightColor,
+                  type: LabelType.h2,
+                  dark: true,
+                  textAlign: TextAlign.center,
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, left: 24, right: 24),
+                  child: Label(
+                    _localizations.no_wallet_desc,
+                    color: application.theme.fontLightColor,
+                    type: LabelType.h4,
+                    dark: true,
+                    softWrap: true,
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
             ),
-            Expanded(
-              flex: 0,
-              child: Container(
-                // color: Colors.green,
-                child: Column(
-                  children: <Widget>[
-                    Label(_localizations.no_wallet_title, type: LabelType.h2, dark: true, textAlign: TextAlign.center),
-                    Padding(
-                      padding: EdgeInsets.only(top: 16, left: 24, right: 24),
-                      child: Label(_localizations.no_wallet_desc, type: LabelType.h4, dark: true, softWrap: true, textAlign: TextAlign.center),
-                    )
-                  ],
+          ),
+          Expanded(
+            flex: 0,
+            child: Column(
+              children: <Widget>[
+                Button(
+                  text: _localizations.no_wallet_create,
+                  fontColor: application.theme.fontLightColor,
+                  backgroundColor: application.theme.primaryColor,
+                  onPressed: () {
+                    Navigator.pushNamed(context, WalletCreateNKNScreen.routeName);
+                  },
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 0,
-              child: Container(
-                // color: Colors.red,
-                child: Column(
-                  children: <Widget>[
-                    Button(
-                      text: _localizations.no_wallet_create,
-                      onPressed: () {
-                        Navigator.pushNamed(context, WalletCreateNKNScreen.routeName);
-                      },
-                    ),
-                    Button(
-                      text: _localizations.no_wallet_import,
-                      backgroundColor: Color(0xFF232D50),
-                      onPressed: () {
-                        // Navigator.pushNamed(context, WalletImportNKNScreen.routeName);
-                      },
-                    ),
-                  ],
+                SizedBox(height: 12),
+                Button(
+                  text: _localizations.no_wallet_import,
+                  fontColor: application.theme.fontLightColor,
+                  backgroundColor: application.theme.primaryColor.withAlpha(20),
+                  onPressed: () {
+                    Navigator.pushNamed(context, WalletImportNKNScreen.routeName);
+                  },
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
