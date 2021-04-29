c2aca2d519185d086358041d546d8aaed7c4f1b2
guo <jiangzhiguo1992@163.com>
2021-04-29 18:20:47 +0800

    add wallet import screen and tabs component

--- lib/routes/wallet.dart
+++ lib/routes/wallet.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/screens/wallet/create_nkn.dart';
 import 'package:nmobile/screens/wallet/home.dart';
+import 'package:nmobile/screens/wallet/import.dart';
 
 Map<String, WidgetBuilder> routes = {
   WalletHomeScreen.routeName: (BuildContext context) => WalletHomeScreen(),
   WalletCreateNKNScreen.routeName: (BuildContext context) => WalletCreateNKNScreen(),
+  WalletImportScreen.routeName: (BuildContext context) => WalletImportScreen(),
 };
 
 init() {
