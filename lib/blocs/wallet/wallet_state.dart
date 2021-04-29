9a1ec9ed8cfa6f11a72d982bf3790c985753e08f
heron <peng.liu@nkn.org>
2021-04-29 19:23:10 +0800

    add wallet bloc
    Signed-off-by: heron <peng.liu@nkn.org>

--- lib/blocs/wallet/wallet_state.dart
+++ lib/blocs/wallet/wallet_state.dart
@@ -1,39 +1,15 @@
 part of 'wallet_bloc.dart';
 
-abstract class WalletState extends Equatable {
+abstract class WalletState {
   const WalletState();
-
-  @override
-  List<Object> get props => [];
-}
-
-// init
-class WalletStateInitial extends WalletState {
-  @override
-  String toString() {
-    return 'WalletInitial{}';
-  }
 }
 
 // loading
-class WalletStateLoading extends WalletState {
-  @override
-  String toString() {
-    return 'WalletLoading{}';
-  }
-}
+class Loading extends WalletState {}
 
 // loaded
-class WalletStateLoaded extends WalletState {
+class Loaded extends WalletState {
   final List<WalletSchema> wallets;
 
-  WalletStateLoaded(this.wallets);
-
-  @override
-  List<Object> get props => [wallets];
-
-  @override
-  String toString() {
-    return 'WalletStateLoaded{wallets: $wallets}';
-  }
+  Loaded(this.wallets);
 }
