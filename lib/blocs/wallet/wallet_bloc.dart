7f3d2f08ca68140a91519e612b83a8b0e48e2798
guo <jiangzhiguo1992@163.com>
2021-04-29 11:27:51 +0800

    move wallet scheme

--- lib/blocs/wallet/wallet_bloc.dart
+++ lib/blocs/wallet/wallet_bloc.dart
@@ -3,7 +3,8 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:nmobile/model/entity/wallet.dart';
+import 'package:nmobile/schema/wallet.dart';
+import 'package:nmobile/utils/logger.dart';
 
 part 'wallet_event.dart';
 part 'wallet_state.dart';
@@ -18,14 +19,22 @@ class WalletBloc extends Bloc<WalletEvent, WalletState> {
   @override
   Stream<WalletState> mapEventToState(WalletEvent event) async* {
     // TODO:GG implement mapEventToState
+    logger.i("wallet_bloc " + event.toString());
     if (event is WalletEventLoad) {
       yield* _onLoadedWallet(event);
     } else if (event is WalletEventReload) {
+      yield* _onLoadedWallet(event);
     } else if (event is WalletEventAdd) {
+      yield* _onLoadedWallet(event);
     } else if (event is WalletEventDel) {
+      yield* _onLoadedWallet(event);
     } else if (event is WalletEventUpd) {
+      yield* _onLoadedWallet(event);
     } else if (event is WalletEventUpdBalance) {
-    } else if (event is WalletEventUpdBackedUp) {}
+      yield* _onLoadedWallet(event);
+    } else if (event is WalletEventUpdBackedUp) {
+      yield* _onLoadedWallet(event);
+    }
   }
 
   Stream<WalletState> _onLoadedWallet(event) async* {
@@ -34,6 +43,7 @@ class WalletBloc extends Bloc<WalletEvent, WalletState> {
     // TODO:GG 2.先check下开启app后有没有拉取过，有就走3，没有就从SQLite里获取Wallet，然后放至SP里
     // TODO:GG 3.从SP里获取wallets，再校验数据做容错处理
     var wallets = [testWallet, testWallet];
+    logger.i("_onLoadedWallet " + wallets.toString());
     yield WalletStateLoaded(wallets);
   }
 }
