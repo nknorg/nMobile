18293d9f5c9958a803ef5cf781ffbf718b37f85a
guo <jiangzhiguo1992@163.com>
2021-04-30 16:51:58 +0800

    add wallet home list screen

--- lib/routes/wallet.dart
+++ lib/routes/wallet.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/screens/wallet/create_nkn.dart';
 import 'package:nmobile/screens/wallet/home.dart';
+import 'package:nmobile/screens/wallet/home_list.dart';
 import 'package:nmobile/screens/wallet/import.dart';
 
 Map<String, WidgetBuilder> routes = {
   WalletHomeScreen.routeName: (BuildContext context) => WalletHomeScreen(),
   WalletCreateNKNScreen.routeName: (BuildContext context) => WalletCreateNKNScreen(),
   WalletImportScreen.routeName: (BuildContext context) => WalletImportScreen(),
+  WalletHomeListScreen.routeName: (BuildContext context) => WalletHomeListScreen(),
 };
 
 init() {
