8f8f33d47492dfff08c147883cc4c9c2a53fbb2f
guo <jiangzhiguo1992@163.com>
2021-05-08 19:21:49 +0800

    add wallet export nkn screen

--- lib/routes/wallet.dart
+++ lib/routes/wallet.dart
@@ -3,6 +3,7 @@ import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/screens/wallet/create_eth.dart';
 import 'package:nmobile/screens/wallet/create_nkn.dart';
 import 'package:nmobile/screens/wallet/detail_nkn.dart';
+import 'package:nmobile/screens/wallet/export_nkn.dart';
 import 'package:nmobile/screens/wallet/home.dart';
 import 'package:nmobile/screens/wallet/import.dart';
 
@@ -12,6 +13,7 @@ Map<String, WidgetBuilder> routes = {
   WalletCreateETHScreen.routeName: (BuildContext context) => WalletCreateETHScreen(),
   WalletImportScreen.routeName: (BuildContext context, {arguments}) => WalletImportScreen(arguments: arguments),
   WalletDetailNKNScreen.routeName: (BuildContext context, {arguments}) => WalletDetailNKNScreen(arguments: arguments),
+  WalletExportNKNScreen.routeName: (BuildContext context, {arguments}) => WalletExportNKNScreen(arguments: arguments),
 };
 
 init() {
