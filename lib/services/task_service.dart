7535c193152010d9208b77f87f356759109c644e
guo <jiangzhiguo1992@163.com>
2021-05-12 11:03:31 +0800

    add wallets balance query task

--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -1,11 +1,22 @@
 import 'dart:async';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nkn_sdk_flutter/wallet.dart';
+import 'package:nmobile/blocs/wallet/wallet_bloc.dart';
+import 'package:nmobile/common/global.dart';
+import 'package:nmobile/schema/wallet.dart';
+import 'package:nmobile/utils/logger.dart';
+
 class TaskService {
   bool _isInit = false;
+  WalletBloc _walletBloc;
+  // EthErc20Client _erc20client;
   Timer _queryWalletBalanceTask;
 
   install() {
     if (!_isInit) {
+      _walletBloc = BlocProvider.of<WalletBloc>(Global.appContext);
+      // _erc20client = EthErc20Client();
       _queryWalletBalanceTask = Timer.periodic(Duration(seconds: 60), (timer) {
         queryWalletBalanceTask();
       });
@@ -19,6 +30,40 @@ class TaskService {
   }
 
   queryWalletBalanceTask() {
-    // TODO:GG wallet_balance
+    var state = _walletBloc.state;
+    if (state is WalletLoaded) {
+      logger.d("queryNknWalletBalanceTask begin");
+      List<Future> futures = <Future>[];
+      state.wallets?.forEach((w) {
+        if (w?.type == WalletType.eth) {
+          // TODO:GG eth balance query
+          // futures.add(_erc20client.getBalance(address: w.address).then((balance) {
+          //   NLog.w('Get Wallet:${w.name} | balance: ${balance.ether}');
+          //   w.balanceEth = balance.ether;
+          //   _walletsBloc.add(UpdateWallet(w));
+          // }));
+          // futures.add(
+          //     _erc20client.getNknBalance(address: w.address).then((balance) {
+          //       if (balance != null) {
+          //         NLog.w('Get Wallet:${w.name} | balance: ${balance.ether}');
+          //         w.balance = balance.ether;
+          //         _walletsBloc.add(UpdateWallet(w));
+          //       }
+          //     }));
+        } else {
+          Wallet.getBalanceByAddr(w?.address ?? "").then((balance) {
+            logger.d("wallet balance  address:${w?.address} - balance_old:${w?.balance} - balance_new:$balance");
+            if (w?.balance != balance) {
+              w?.balance = balance;
+              _walletBloc.add(UpdateWallet(w));
+            }
+          });
+        }
+      });
+      // TODO:GG need?
+      // Future.wait(futures).then((data) {
+      //   _walletBloc.add(ReLoadWallets());
+      // });
+    }
   }
 }
