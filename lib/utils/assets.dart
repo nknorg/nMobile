39f2cfa45077dfc3516a0748cbe28d4550581c46
heron <peng.liu@nkn.org>
2021-05-13 15:26:39 +0800

    Add private message
    Signed-off-by: heron <peng.liu@nkn.org>

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mime_type/mime_type.dart';
+import 'package:nmobile/common/global.dart';
+import 'package:path/path.dart';
+
+import 'path.dart';
 
 Widget assetIcon(String name, {double width, Color color}) {
   return SvgPicture.asset(
@@ -18,4 +27,50 @@ Widget assetImage(String path, {double width, double height, BoxFit fit, Color c
     fit: fit,
     color: color,
   );
+}
+
+String createRandomWebPFile(String accountPubkey) {
+  var value = new DateTime.now().millisecondsSinceEpoch.toString();
+  Directory rootDir = Global.applicationRootDirectory;
+  Directory dir = Directory(join(rootDir.path, accountPubkey));
+  if (!dir.existsSync()) {
+    dir.createSync(recursive: true);
+  }
+  String path = join(rootDir.path, dir.path, value.toString() + '_temp.jpeg');
+  return path;
+}
+
+Future<File> compressAndGetFile(String accountPubkey, File file) async {
+  final dir = Global.applicationRootDirectory;
+
+  final targetPath = createRandomWebPFile(accountPubkey);
+  if (!dir.existsSync()) {
+    dir.createSync(recursive: true);
+  }
+  var result = await FlutterImageCompress.compressAndGetFile(file.absolute.path, targetPath, quality: 30, minWidth: 640, minHeight: 1024, format: CompressFormat.jpeg);
+  return result;
+}
+
+Future<File> getCameraFile(String accountPubkey, {@required ImageSource source}) async {
+  File image;
+  final picker = ImagePicker();
+  final pickedFile = await picker.getImage(source: source);
+
+  if(pickedFile != null) {
+    image = File(pickedFile.path);
+  }
+
+  if (image != null) {
+    File savedImg;
+    if (mime(image.path).indexOf('image/gif') > -1) {
+      var path = createFileCachePath(accountPubkey, image);
+      savedImg = image.copySync(path);
+    } else {
+      savedImg = await compressAndGetFile(accountPubkey, image);
+    }
+
+    return savedImg;
+  } else {
+    return null;
+  }
 }
