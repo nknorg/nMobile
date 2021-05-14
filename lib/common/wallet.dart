3fa05fbd79fd4950902228647fd591f0dd98ef14
guo <jiangzhiguo1992@163.com>
2021-05-14 10:31:22 +0800

    refactor common wallet

--- lib/common/wallet.dart
+++ lib/common/wallet.dart
@@ -8,75 +8,78 @@ import 'package:nmobile/utils/logger.dart';
 
 import 'locator.dart';
 
-Future<WalletSchema> getWalletInStorageByAddress(String address) async {
-  if (address == null || address.length == 0) return null;
-  List<WalletSchema> wallets = await WalletStorage().getWallets();
-  if (wallets == null || wallets.isEmpty) return null;
-  return Future(() => wallets.firstWhere((x) => x?.address == address, orElse: () => null));
-}
-
-WalletSchema getWalletInOriginalByAddress(List<WalletSchema> wallets, String address) {
-  if (address == null || address.length == 0) return null;
-  if (wallets == null || wallets.isEmpty) return null;
-  return wallets.firstWhere((x) => x?.address == address, orElse: () => null);
-}
-
-Future<String> getWalletKeystoreByAddress(String address) {
-  return WalletStorage().getKeystore(address);
-}
-
-Future<bool> isWalletsBackup({List original}) async {
+class Wallet {
   WalletStorage _walletStorage = WalletStorage();
-  List wallets = original ?? await _walletStorage.getWallets();
-  // backups
-  List<Future> futures = <Future>[];
-  wallets?.forEach((value) {
-    futures.add(_walletStorage.isBackupByAddress(value?.address));
-  });
-  List backups = await Future.wait(futures);
-  // allBackup
-  logger.d("wallet backup - $backups");
-  bool find = backups?.firstWhere((backup) => backup == false || backup == null, orElse: () => true);
-  bool allBackup = find == true ? true : false;
-  logger.d("wallet backup - allBackup:$allBackup");
-  return allBackup;
-}
 
-Future<WalletSchema> getWalletDefault() async {
-  String address = await getWalletDefaultAddress();
-  return getWalletInStorageByAddress(address);
-}
-
-Future<String> getWalletDefaultAddress() {
-  return WalletStorage().getDefaultAddress();
-}
-
-Future<String> getWalletPassword(BuildContext context, String walletAddress) {
-  if (walletAddress == null || walletAddress.isEmpty) {
-    return Future.value(null);
+  Future<WalletSchema> getWalletInStorageByAddress(String address) async {
+    if (address == null || address.length == 0) return null;
+    List<WalletSchema> wallets = await _walletStorage.getWallets();
+    if (wallets == null || wallets.isEmpty) return null;
+    return Future(() => wallets.firstWhere((x) => x?.address == address, orElse: () => null));
   }
-  S _localizations = S.of(context);
-  return Future(() async {
-    if (Settings.biometricsAuthentication) {
-      return authorization.authenticationIfCan();
-    }
-    return false;
-  }).then((bool authOk) async {
-    String pwd = await WalletStorage().getPassword(walletAddress);
-    if (!authOk || pwd == null || pwd.isEmpty) {
-      return BottomDialog.of(context).showInput(
-        title: _localizations.verify_wallet_password,
-        inputTip: _localizations.wallet_password,
-        inputHint: _localizations.input_password,
-        actionText: _localizations.continue_text,
-        password: true,
-      );
-    }
-    return pwd;
-  });
-}
 
-bool isBalanceSame(WalletSchema w1, WalletSchema w2) {
-  if (w1 == null || w2 == null) return true;
-  return w1.balance == w2.balance && w1.balanceEth == w2.balanceEth;
+  WalletSchema getWalletInOriginalByAddress(List<WalletSchema> wallets, String address) {
+    if (address == null || address.length == 0) return null;
+    if (wallets == null || wallets.isEmpty) return null;
+    return wallets.firstWhere((x) => x?.address == address, orElse: () => null);
+  }
+
+  Future<String> getWalletKeystoreByAddress(String address) {
+    return _walletStorage.getKeystore(address);
+  }
+
+  Future<bool> isWalletsBackup({List original}) async {
+    List wallets = original ?? await _walletStorage.getWallets();
+    // backups
+    List<Future> futures = <Future>[];
+    wallets?.forEach((value) {
+      futures.add(_walletStorage.isBackupByAddress(value?.address));
+    });
+    List backups = await Future.wait(futures);
+    // allBackup
+    logger.d("wallet backup - $backups");
+    bool find = backups?.firstWhere((backup) => backup == false || backup == null, orElse: () => true);
+    bool allBackup = find == true ? true : false;
+    logger.d("wallet backup - allBackup:$allBackup");
+    return allBackup;
+  }
+
+  Future<WalletSchema> getWalletDefault() async {
+    String address = await getWalletDefaultAddress();
+    return getWalletInStorageByAddress(address);
+  }
+
+  Future<String> getWalletDefaultAddress() {
+    return _walletStorage.getDefaultAddress();
+  }
+
+  Future<String> getWalletPassword(BuildContext context, String walletAddress) {
+    if (walletAddress == null || walletAddress.isEmpty) {
+      return Future.value(null);
+    }
+    S _localizations = S.of(context);
+    return Future(() async {
+      if (Settings.biometricsAuthentication) {
+        return authorization.authenticationIfCan();
+      }
+      return false;
+    }).then((bool authOk) async {
+      String pwd = await _walletStorage.getPassword(walletAddress);
+      if (!authOk || pwd == null || pwd.isEmpty) {
+        return BottomDialog.of(context).showInput(
+          title: _localizations.verify_wallet_password,
+          inputTip: _localizations.wallet_password,
+          inputHint: _localizations.input_password,
+          actionText: _localizations.continue_text,
+          password: true,
+        );
+      }
+      return pwd;
+    });
+  }
+
+  bool isBalanceSame(WalletSchema w1, WalletSchema w2) {
+    if (w1 == null || w2 == null) return true;
+    return w1.balance == w2.balance && w1.balanceEth == w2.balanceEth;
+  }
 }
