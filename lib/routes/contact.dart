ace56ba1f494da9d295077b33da191941bebf19b
guo <jiangzhiguo1992@163.com>
2021-05-17 11:03:24 +0800

    format routes

--- lib/routes/contact.dart
+++ lib/routes/contact.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/screens/contact/home.dart';
 
-Map<String, WidgetBuilder> routes = {
+Map<String, WidgetBuilder> _routes = {
   ContactHomeScreen.routeName: (BuildContext context, {arguments}) => ContactHomeScreen(arguments: arguments),
 };
 
 init() {
-  application.registerRoutes(routes);
+  application.registerRoutes(_routes);
 }
