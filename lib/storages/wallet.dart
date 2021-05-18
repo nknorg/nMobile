ba30e6d13c4a33e09371786438e09537bf760014
guo <jiangzhiguo1992@163.com>
2021-05-18 17:03:52 +0800

    rename wallet storage field

--- lib/storages/wallet.dart
+++ lib/storages/wallet.dart
@@ -8,8 +8,8 @@ import '../helpers/local_storage.dart';
 import '../helpers/secure_storage.dart';
 
 class WalletStorage {
-  static const String KEY_WALLETS = 'WALLETS';
-  static const String KEY_KEYSTORE = 'KEYSTORE';
+  static const String KEY_WALLET = 'WALLETS';
+  static const String KEY_KEYSTORE = 'KEYSTORES';
   static const String KEY_PASSWORD = 'PASSWORDS';
   static const String KEY_SEED = 'SEED';
   static const String KEY_BACKUP = 'BACKUP';
@@ -19,7 +19,7 @@ class WalletStorage {
   final SecureStorage _secureStorage = SecureStorage();
 
   Future<List> getWallets() async {
-    var wallets = await _localStorage.getArray(KEY_WALLETS);
+    var wallets = await _localStorage.getArray(KEY_WALLET);
     if (wallets != null && wallets.isNotEmpty) {
       final list = wallets.map((e) {
         WalletSchema walletSchema = WalletSchema.fromCacheMap(e);
@@ -32,12 +32,12 @@ class WalletStorage {
 
   Future addWallet(WalletSchema walletSchema, String keystore, {String password, String seed}) async {
     List<Future> futures = <Future>[];
-    var wallets = await _localStorage.getArray(KEY_WALLETS);
+    var wallets = await _localStorage.getArray(KEY_WALLET);
     int index = wallets?.indexWhere((x) => x['address'] == walletSchema?.address) ?? -1;
     if (index < 0) {
-      futures.add(_localStorage.addItem(KEY_WALLETS, walletSchema?.toCacheMap()));
+      futures.add(_localStorage.addItem(KEY_WALLET, walletSchema?.toCacheMap()));
     } else {
-      futures.add(_localStorage.setItem(KEY_WALLETS, index, walletSchema?.toCacheMap()));
+      futures.add(_localStorage.setItem(KEY_WALLET, index, walletSchema?.toCacheMap()));
     }
     if (keystore != null && keystore.isNotEmpty) {
       if (Platform.isAndroid) {
@@ -58,7 +58,7 @@ class WalletStorage {
   Future deleteWallet(int n, WalletSchema walletSchema) async {
     List<Future> futures = <Future>[];
     if (n >= 0) {
-      futures.add(_localStorage.removeItem(KEY_WALLETS, n));
+      futures.add(_localStorage.removeItem(KEY_WALLET, n));
       if (Platform.isAndroid) {
         futures.add(_localStorage.remove('$KEY_KEYSTORE:${walletSchema?.address}'));
       } else {
@@ -77,7 +77,7 @@ class WalletStorage {
   Future updateWallet(int n, WalletSchema walletSchema, {String keystore, String password, String seed}) {
     List<Future> futures = <Future>[];
     if (n >= 0) {
-      futures.add(_localStorage.setItem(KEY_WALLETS, n, walletSchema?.toCacheMap()));
+      futures.add(_localStorage.setItem(KEY_WALLET, n, walletSchema?.toCacheMap()));
       if (keystore != null && keystore.isNotEmpty) {
         if (Platform.isAndroid) {
           futures.add(_localStorage.set('$KEY_KEYSTORE:${walletSchema?.address}', keystore));
