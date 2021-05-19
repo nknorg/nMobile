e9b199fca0d6afb8ead2c52711b295406352fc9e
guo <jiangzhiguo1992@163.com>
2021-05-19 18:23:32 +0800

    update contact save action on add screen

--- lib/routes/contact.dart
+++ lib/routes/contact.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:nmobile/common/locator.dart';
+import 'package:nmobile/screens/contact/add.dart';
 import 'package:nmobile/screens/contact/home.dart';
 
 Map<String, WidgetBuilder> _routes = {
   ContactHomeScreen.routeName: (BuildContext context, {arguments}) => ContactHomeScreen(arguments: arguments),
+  ContactAddScreen.routeName: (BuildContext context) => ContactAddScreen(),
 };
 
 init() {
