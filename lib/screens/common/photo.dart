030f043e68864efe8969318dd9cabb172a275b9b
guo <jiangzhiguo1992@163.com>
2021-05-21 14:52:09 +0800

    add photo view screen

---
+++ lib/screens/common/photo.dart
@@ -0,0 +1,115 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:nmobile/components/layout/header.dart';
+import 'package:nmobile/components/layout/layout.dart';
+import 'package:nmobile/generated/l10n.dart';
+import 'package:photo_view/photo_view.dart';
+
+class PhotoScreen extends StatefulWidget {
+  static final String routeName = "/photo";
+  static final String argFilePath = "file_path";
+  static final String argNetUrl = "net_url";
+
+  static Future go(BuildContext context, {String filePath, String netUrl}) {
+    if ((filePath == null || filePath.isEmpty) && (netUrl == null || netUrl.isEmpty)) return null;
+    return Navigator.pushNamed(context, routeName, arguments: {
+      argFilePath: filePath,
+      argNetUrl: netUrl,
+    });
+  }
+
+  final Map<String, dynamic> arguments;
+
+  PhotoScreen({Key key, this.arguments}) : super(key: key);
+
+  @override
+  _PhotoScreenState createState() => _PhotoScreenState();
+}
+
+class _PhotoScreenState extends State<PhotoScreen> with SingleTickerProviderStateMixin {
+  static const int TYPE_FILE = 1;
+  static const int TYPE_NET = 2;
+
+  int _contentType;
+  String _content;
+
+  @override
+  void initState() {
+    super.initState();
+    String filePath = widget.arguments[PhotoScreen.argFilePath];
+    String netUrl = widget.arguments[PhotoScreen.argNetUrl];
+    if (filePath != null && filePath.isNotEmpty) {
+      _contentType = TYPE_FILE;
+      _content = filePath;
+    } else if (netUrl != null && netUrl.isNotEmpty) {
+      _contentType = TYPE_NET;
+      _content = netUrl;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    S _localizations = S.of(context);
+
+    return Layout(
+      headerColor: Colors.black,
+      borderRadius: BorderRadius.zero,
+      header: Header(
+        backgroundColor: Colors.black,
+        // TODO:GG image save to album
+        // actions: [
+        //   PopupMenuButton(
+        //     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        //     icon: Asset.iconSvg('more', width: 24),
+        //     onSelected: (int result) async {
+        //       switch (result) {
+        //         case 0:
+        //           // File file = File(widget.arguments);
+        //           // print(file.path);
+        //           // bool exist = await file.exists();
+        //           // if (exist) {
+        //           //   try {
+        //           //     String name = 'nkn_' + DateTime.now().millisecondsSinceEpoch.toString();
+        //           //     final Uint8List bytes = await file.readAsBytes();
+        //           //     bool success = await ImageSave.saveImage(bytes, 'jpeg', albumName: name);
+        //           //     if (success) {
+        //           //       showToast(NL10ns.of(context).success);
+        //           //     } else {
+        //           //       showToast(NL10ns.of(context).failure);
+        //           //     }
+        //           //   } catch (e) {
+        //           //     showToast(NL10ns.of(context).failure);
+        //           //   }
+        //           // } else {
+        //           //   showToast(NL10ns.of(context).failure);
+        //           // }
+        //           break;
+        //       }
+        //     },
+        //     itemBuilder: (BuildContext context) => <PopupMenuEntry<int>>[
+        //       PopupMenuItem<int>(
+        //         value: 0,
+        //         child: Label(
+        //           _localizations.save_to_album,
+        //           type: LabelType.display,
+        //         ),
+        //       ),
+        //     ],
+        //   )
+        // ],
+      ),
+      body: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: PhotoView(
+            imageProvider: this._contentType == TYPE_FILE
+                ? FileImage(File(this._content))
+                : this._contentType == TYPE_NET
+                    ? Image.network(this._content)
+                    : null,
+          )),
+    );
+  }
+}
