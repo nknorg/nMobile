e2f86040f7ea73ceaa13596540aa5a968738c2c4
guo <jiangzhiguo1992@163.com>
2021-05-25 14:57:20 +0800

    rename settings screen

--- lib/routes/settings.dart
+++ lib/routes/settings.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/screens/settings/cache.dart';
-import 'package:nmobile/screens/settings/settings.dart';
+import 'package:nmobile/screens/settings/home.dart';
 
 Map<String, WidgetBuilder> _routes = {
-  SettingsScreen.routeName: (BuildContext context) => SettingsScreen(),
-  CacheScreen.routeName: (BuildContext context) => CacheScreen(),
+  SettingsHomeScreen.routeName: (BuildContext context) => SettingsHomeScreen(),
+  SettingsCacheScreen.routeName: (BuildContext context) => SettingsCacheScreen(),
 };
 
 init() {
