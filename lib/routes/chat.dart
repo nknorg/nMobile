0de007cc9419cc51c843cdab0496d903ea58d3b8
guo <jiangzhiguo1992@163.com>
2021-05-25 18:55:12 +0800

    rename chat messages screen

--- lib/routes/chat.dart
+++ lib/routes/chat.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/screens/chat/home.dart';
-import 'package:nmobile/screens/chat/messages_detail.dart';
+import 'package:nmobile/screens/chat/messages.dart';
 
 Map<String, WidgetBuilder> _routes = {
   ChatHomeScreen.routeName: (BuildContext context) => ChatHomeScreen(),
-  ChatMessagesDetailScreen.routeName: (BuildContext context, {arguments}) => ChatMessagesDetailScreen(arguments: arguments),
+  ChatMessagesScreen.routeName: (BuildContext context, {arguments}) => ChatMessagesScreen(arguments: arguments),
 };
 
 init() {
