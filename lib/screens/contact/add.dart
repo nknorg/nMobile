b28b66ccfd70b74cb474c0dac5682d3a25158252
guo <jiangzhiguo1992@163.com>
2021-05-25 18:43:33 +0800

    rename some chat class

--- lib/screens/contact/add.dart
+++ lib/screens/contact/add.dart
@@ -274,7 +274,7 @@ class ContactAddScreenState extends State<ContactAddScreen> {
                           controller: _walletAddressController,
                           focusNode: _walletAddressFocusNode,
                           hintText: _localizations.input_wallet_address,
-                          validator: Validator.of(context).addressNKN(),
+                          validator: Validator.of(context).addressNKN(), // TODO:GG or empty
                           textInputAction: TextInputAction.next,
                           onFieldSubmitted: (_) {
                             FocusScope.of(context).requestFocus(_notesFocusNode);
