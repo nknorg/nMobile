16749291bd6da68de59611d0d7fdc42e4b59fa23
heron <peng.liu@nkn.org>
2021-05-29 19:06:13 +0800

    Add receive media
    Signed-off-by: heron <peng.liu@nkn.org>

--- lib/schema/message.dart
+++ lib/schema/message.dart
@@ -503,7 +503,7 @@ class MessageSchema extends Equatable {
     return map;
   }
 
-  Future<File?> getMediaFile() async {
+  Future<File?> LoadMediaFile() async {
     if (content == null) return null;
     var match = RegExp(r'\(data:(.*);base64,(.*)\)').firstMatch(content);
     var mimeType = match?.group(1) ?? "";
@@ -524,7 +524,7 @@ class MessageSchema extends Equatable {
     } else if (mimeType.indexOf('aac') > -1) {
       extension = 'aac';
     } else {
-      logger.w('getMediaFile - no_extension');
+      logger.w('LoadMediaFile - no_extension');
     }
 
     var bytes = base64Decode(fileBase64);
@@ -532,11 +532,13 @@ class MessageSchema extends Equatable {
     String localPath = Path.createLocalChatFile(hexEncode(chatCommon.publicKey!), '$name.$extension');
     File file = File(Path.getCompleteFile(localPath));
 
-    logger.d('getMediaFile - path:${file.absolute}');
+    logger.d('LoadMediaFile - path:${file.absolute}');
 
     if (!await file.exists()) {
-      logger.d('getMediaFile - write:${file.absolute}');
+      file.createSync(recursive: true);
+      logger.d('LoadMediaFile - write:${file.absolute}');
       await file.writeAsBytes(bytes, flush: true);
+      content = file;
     }
     return file;
   }
