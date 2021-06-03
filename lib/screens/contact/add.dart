b6b0bae20772698b3a05e92212d86a3d53b4bc17
guo <jiangzhiguo1992@163.com>
2021-06-03 17:29:15 +0800

    update message pieces receive

--- lib/screens/contact/add.dart
+++ lib/screens/contact/add.dart
@@ -59,7 +59,7 @@ class ContactAddScreenState extends State<ContactAddScreen> with Tag {
 
   _selectAvatarPicture() async {
     if (chatCommon.publicKey == null) return;
-    String returnPath = Path.getCompleteFile(Path.createLocalContactFile(hexEncode(chatCommon.publicKey!), "${Uuid().v4()}.jpeg"));
+    String returnPath = Path.getCompleteFile(Path.createLocalFile(hexEncode(chatCommon.publicKey!), SubDirType.contact, "${Uuid().v4()}.jpeg"));
     File? picked = await MediaPicker.pick(
       mediaType: MediaType.image,
       source: ImageSource.gallery,
