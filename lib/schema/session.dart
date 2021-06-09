041dd5d3872cf0b4cb925c0ef264a59b07f95dba
guo <jiangzhiguo1992@163.com>
2021-06-09 14:09:26 +0800

    add session schema

--- lib/schema/session.dart
+++ lib/schema/session.dart
@@ -1,52 +1,77 @@
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
-import 'package:nmobile/storages/contact.dart';
+import 'package:nmobile/common/locator.dart';
+import 'package:nmobile/schema/message.dart';
+import 'package:nmobile/storages/message.dart';
 import 'package:nmobile/storages/topic.dart';
+import 'package:nmobile/utils/utils.dart';
 
 import 'contact.dart';
 import 'topic.dart';
 
 class SessionSchema extends Equatable {
   String targetId;
-  String? sender;
-  String? contentType;
-  String? content;
+  bool isTopic = false;
   DateTime? lastReceiveTime;
-  int? notReadCount;
+  Map<String, dynamic>? lastReceiveOptions;
+  int unReadCount;
+  bool isTop;
 
   TopicSchema? topic;
   ContactSchema? contact;
-  bool isTop = false;
+  MessageSchema? message;
 
   SessionSchema({
     required this.targetId,
-    this.sender,
-    this.contentType,
-    this.content,
+    required this.isTopic,
     this.lastReceiveTime,
-    this.notReadCount,
+    this.lastReceiveOptions,
+    this.unReadCount = 0,
+    this.isTop = false,
   });
 
   @override
   List<Object?> get props => [targetId];
 
+  Future<Map<String, dynamic>> toMap() async {
+    Map<String, dynamic> map = {
+      'target_id': targetId,
+      'is_topic': isTopic ? 1 : 0,
+      'last_receive_time': lastReceiveTime?.millisecondsSinceEpoch,
+      'un_read_count': unReadCount,
+      'is_top': isTop ? 1 : 0,
+    };
+    Map<String, dynamic>? options = message?.toMap();
+    map["last_receive_options"] = options != null ? jsonEncode(options) : '{}';
+    return map;
+  }
+
   static Future<SessionSchema?> fromMap(Map e) async {
-    var res = SessionSchema(
+    var schema = SessionSchema(
       targetId: e['target_id'] ?? "",
-      sender: e['sender'],
-      contentType: e['type'],
-      content: e['content'],
-      lastReceiveTime: DateTime.fromMillisecondsSinceEpoch(e['receive_time']),
-      notReadCount: e['not_read'] ?? 0,
+      isTopic: (e['is_topic'] != null && e['is_topic'] == 1) ? true : false,
+      lastReceiveTime: e['last_receive_time'] != null ? DateTime.fromMillisecondsSinceEpoch(e['last_receive_time']) : null,
+      lastReceiveOptions: e['last_receive_options'] != null ? jsonFormat(e['last_receive_options']) : null,
+      unReadCount: e['un_read_count'] ?? 0,
+      isTop: (e['is_top'] != null && e['is_top'] == 1) ? true : false,
     );
-    if (res.targetId.isEmpty) return null;
-    if (e['topic'] != null) {
-      res.topic = await TopicStorage().queryTopicByTopicName(e['topic']);
-      res.contact = await ContactStorage().queryByClientAddress(res.targetId);
-      res.isTop = res.topic?.isTop ?? false;
+    if (schema.targetId.isEmpty) return null;
+    // topic + contact
+    if (schema.isTopic) {
+      schema.topic = await TopicStorage().queryTopicByTopicName(schema.targetId);
     } else {
-      res.contact = await ContactStorage().queryByClientAddress(res.targetId);
-      res.isTop = res.contact?.isTop ?? false;
+      schema.contact = await contactCommon.queryByClientAddress(schema.targetId);
     }
-    return res;
+    // message
+    if (schema.lastReceiveOptions != null && schema.lastReceiveOptions!.isNotEmpty) {
+      schema.message = MessageSchema.fromMap(schema.lastReceiveOptions!);
+    } else {
+      List<MessageSchema> history = await MessageStorage().queryListCanReadByTargetId(schema.targetId, offset: 0, limit: 1);
+      if (history.isNotEmpty) {
+        schema.message = history[0];
+      }
+    }
+    return schema;
   }
 }
