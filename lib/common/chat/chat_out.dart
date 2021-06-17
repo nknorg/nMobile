4c6636f6ee622f94e56d8efc4af01b143c775fb6
guo <jiangzhiguo1992@163.com>
2021-06-17 11:17:54 +0800

    fixed wallet toggle and piece upload progress

--- lib/common/chat/chat_out.dart
+++ lib/common/chat/chat_out.dart
@@ -31,7 +31,7 @@ class ChatOutCommon with Tag {
   // ignore: close_sinks
   StreamController<Map<String, dynamic>> _onPieceOutController = StreamController<Map<String, dynamic>>.broadcast();
   StreamSink<Map<String, dynamic>> get _onPieceOutSink => _onPieceOutController.sink;
-  Stream<Map<String, dynamic>> get onPieceOutStream => _onPieceOutController.stream.distinct((prev, next) => prev['percent'] >= next['percent']);
+  Stream<Map<String, dynamic>> get onPieceOutStream => _onPieceOutController.stream.distinct((prev, next) => (prev['msg_id'] == prev['next']) && (next['percent'] < prev['percent']));
 
   MessageStorage _messageStorage = MessageStorage();
 
