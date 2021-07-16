c312d7d02cc52c2d153f06bc5a1fb5f3b4f5db8c
guo <jiangzhiguo1992@163.com>
2021-07-16 17:21:27 +0800

    remove flex:0

--- lib/screens/contact/add.dart
+++ lib/screens/contact/add.dart
@@ -320,26 +320,23 @@ class ContactAddScreenState extends State<ContactAddScreen> with Tag {
                 ),
               ),
             ),
-            Expanded(
-              flex: 0,
-              child: SafeArea(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 20),
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30),
-                        child: Button(
-                          text: _localizations.save_contact,
-                          width: double.infinity,
-                          disabled: !_formValid,
-                          onPressed: () {
-                            _saveContact(context);
-                          },
-                        ),
+            SafeArea(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30),
+                      child: Button(
+                        text: _localizations.save_contact,
+                        width: double.infinity,
+                        disabled: !_formValid,
+                        onPressed: () {
+                          _saveContact(context);
+                        },
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
