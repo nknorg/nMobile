af37a5ae1e92fd7eabb5e3c18549520c4ca1c04a
guo <jiangzhiguo1992@163.com>
2021-07-19 15:03:41 +0800

    update subscribers

--- lib/common/topic/subscriber.dart
+++ lib/common/topic/subscriber.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 
 import 'package:nmobile/helpers/error.dart';
 import 'package:nmobile/schema/subscriber.dart';
+import 'package:nmobile/schema/topic.dart';
 import 'package:nmobile/storages/subscriber.dart';
 import 'package:nmobile/utils/logger.dart';
 import 'package:nmobile/utils/utils.dart';
@@ -32,7 +33,47 @@ class SubscriberCommon with Tag {
     _updateController.close();
   }
 
-  Future getSubscribers(
+  Future onInvitee(String? topicName, String? clientAddress) async {
+    if (topicName == null || topicName.isEmpty || clientAddress == null || clientAddress.isEmpty) return;
+    // db exist
+    TopicSchema? exists = await topicCommon.queryByTopic(topicName);
+    if (exists == null) {
+      logger.w("$TAG - onInvitee - empty - schema:$exists");
+      return;
+    }
+    if (exists.isPrivate == true && exists.isOwner(clientCommon.address) == true) {
+      // TODO:GG topic + subers permissions
+    }
+  }
+
+  Future<SubscriberSchema?> onSubscribe(String? topicName, String? clientAddress) async {
+    if (topicName == null || topicName.isEmpty || clientAddress == null || clientAddress.isEmpty) return null;
+    // db
+    SubscriberSchema? subscriber = await subscriberCommon.queryByTopicChatId(topicName, clientAddress);
+    if (subscriber == null) {
+      SubscriberSchema? subscriber = SubscriberSchema.create(topicName, clientAddress, SubscriberStatus.Subscribed);
+      subscriberCommon.add(subscriber, notify: true); // await
+    }
+    if (subscriber == null) return null;
+    // status + perm
+    if (subscriber.status != SubscriberStatus.Subscribed) {
+      bool success = await subscriberCommon.setStatus(subscriber.id, SubscriberStatus.Subscribed, notify: true);
+      if (success) subscriber.status = SubscriberStatus.Subscribed;
+      // TODO:GG subers  permission(owner)
+    }
+    return subscriber;
+  }
+
+  Future<SubscriberSchema?> onUnsubscribe(String? topicName, String? clientAddress) async {
+    if (topicName == null || topicName.isEmpty || clientAddress == null || clientAddress.isEmpty) return null;
+    SubscriberSchema? subscriber = await subscriberCommon.queryByTopicChatId(topicName, clientAddress);
+    if (subscriber == null) return null;
+    // TODO:GG subers  permission(owner)
+    bool success = await delete(subscriber.id, notify: true);
+    return success ? subscriber : null;
+  }
+
+  Future<List<SubscriberSchema>> refreshSubscribers(
     String? topicName, {
     // bool checkJoined = false,
     int offset = 0,
@@ -61,7 +102,7 @@ class SubscriberCommon with Tag {
         }
       }
       if (!dbFindInNode) {
-        await delete(dbItem.id);
+        await delete(dbItem.id, notify: true);
       }
     }
 
@@ -75,16 +116,26 @@ class SubscriberCommon with Tag {
         }
       }
       if (!nodeFindInDB) {
-        await add(SubscriberSchema.create(topicName, nodeItem.clientAddress, nodeItem.status));
+        await add(SubscriberSchema.create(topicName, nodeItem.clientAddress, nodeItem.status), notify: true);
       }
     }
 
     return await queryListByTopic(topicName, offset: offset, limit: limit);
   }
 
-  // TODO:GG call
-  Future getSubscribersCount() async {
-    // TODO:GG 根据subscribers表的成员来校准？
+  Future<int> getSubscribersCount(String? topicName, {bool? isPrivate, Uint8List? subscriberHashPrefix}) async {
+    if (topicName == null || topicName.isEmpty) return 0;
+    bool isPublic = !(isPrivate ?? isPrivateTopicReg(topicName));
+
+    int count = 0;
+    if (isPublic) {
+      count = await this._clientGetSubscribersCount(topicName);
+    } else {
+      count = await this._clientGetSubscribersCount(topicName);
+      // count = await this.queryCountByTopic(topicName);
+      // logger.i("$TAG - getSubscribersCount - node:$test - DB:$count");
+    }
+    return count;
   }
 
   Future<List<SubscriberSchema>> _clientGetSubscribers(
@@ -102,14 +153,13 @@ class SubscriberCommon with Tag {
         topic: genTopicHash(topicName),
         offset: offset,
         limit: limit,
-        meta: meta,
+        meta: true,
         txPool: txPool,
         subscriberHashPrefix: subscriberHashPrefix,
       );
       logger.d("$TAG - _clientGetSubscribers - results:$results");
       results?.forEach((key, value) {
-        // TODO:GG subers ?? status + other
-        var item = SubscriberSchema.create(topicName, key, SubscriberStatus.None);
+        var item = SubscriberSchema.create(topicName, key, SubscriberStatus.None); // TODO:GG subers ?? status + other
         if (item != null) subscribers.add(item);
       });
       logger.d("$TAG - _clientGetSubscribers - subscribers:$subscribers");
@@ -134,8 +184,7 @@ class SubscriberCommon with Tag {
     return count ?? 0;
   }
 
-  // TODO:GG call
-  Future<SubscriberSchema?> add(SubscriberSchema? schema, {bool checkDuplicated = true}) async {
+  Future<SubscriberSchema?> add(SubscriberSchema? schema, {bool topicCountCheck = true, bool notify = false, bool checkDuplicated = true}) async {
     if (schema == null || schema.topic.isEmpty) return null;
     if (checkDuplicated) {
       SubscriberSchema? exist = await queryByTopicChatId(schema.topic, schema.clientAddress);
@@ -145,23 +194,34 @@ class SubscriberCommon with Tag {
       }
     }
     SubscriberSchema? added = await _subscriberStorage.insert(schema);
-    if (added != null) _addSink.add(added);
+    if (added != null && notify) _addSink.add(added);
+
+    // topic count
+    if (topicCountCheck) {
+      topicCommon.queryByTopic(schema.topic).then((value) async {
+        if (value != null) {
+          // int count = await getSubscribersCount(schema.topic);
+          // if (value.count != count) {
+          //   topicCommon.setCount(value.id, count); // await
+          // }
+        }
+      });
+    }
     return added;
   }
 
   // TODO:GG call
   Future<bool> delete(int? subscriberId, {bool notify = false}) async {
     if (subscriberId == null || subscriberId == 0) return false;
-    bool deleted = await _subscriberStorage.delete(subscriberId);
-    if (deleted) _deleteSink.add(subscriberId);
-    return deleted;
+    bool success = await _subscriberStorage.delete(subscriberId);
+    if (success && notify) _deleteSink.add(subscriberId);
+    return success;
   }
 
   // TODO:GG call
   Future<int> deleteByTopic(String? topic) async {
     if (topic == null || topic.isEmpty) return 0;
     int count = await _subscriberStorage.deleteByTopic(topic);
-    // if (deleted) _deleteSink.add(subscriberId);
     return count;
   }
 
@@ -178,11 +238,16 @@ class SubscriberCommon with Tag {
     return _subscriberStorage.queryListByTopic(topic, status: status, orderBy: orderBy, offset: offset, limit: limit);
   }
 
+  // TODO:GG call
   Future<List<SubscriberSchema>> queryListByTopicPerm(String? topic, int? permPage) {
     return _subscriberStorage.queryListByTopicPerm(topic, permPage);
   }
 
   // TODO:GG call
+  Future<int> queryCountByTopic(String? topic, {int? status}) {
+    return _subscriberStorage.queryCountByTopic(topic, status: status);
+  }
+
   Future<bool> setStatus(int? subscriberId, int? status, {bool notify = false}) async {
     if (subscriberId == null || subscriberId == 0) return false;
     bool success = await _subscriberStorage.setStatus(subscriberId, status);
@@ -200,7 +265,7 @@ class SubscriberCommon with Tag {
 
   Future queryAndNotify(int? subscriberId) async {
     if (subscriberId == null || subscriberId == 0) return;
-    SubscriberSchema? updated = await _subscriberStorage.query(subscriberId);
+    SubscriberSchema? updated = await query(subscriberId);
     if (updated != null) {
       _updateSink.add(updated);
     }
