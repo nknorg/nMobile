dcb9087801d8b67a5f03dc8963e34219e9225bde
guo <jiangzhiguo1992@163.com>
2021-08-03 14:33:50 +0800

    update logger

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
+
 import 'package:logger/logger.dart';
 
 Logger logger = Logger(
   filter: null,
-  printer: SimplePrinter(),
+  printer: ColorPrinter(),
   output: null,
   level: Level.verbose,
 );
@@ -17,5 +19,55 @@ mixin Tag {
     //   return name.substring(name.length - length);
     // } else
     //   return name.padLeft(length, '.');
+  }
+}
+
+class ColorPrinter extends LogPrinter {
+  static final levelPrefixes = {
+    Level.verbose: '[V]',
+    Level.debug: '[D]',
+    Level.info: '[I]',
+    Level.warning: '[W]',
+    Level.error: '[E]',
+    Level.wtf: '[WTF]',
+  };
+
+  static final levelColors = {
+    Level.verbose: AnsiColor.fg(AnsiColor.grey(0.5)),
+    Level.debug: AnsiColor.none(),
+    Level.info: AnsiColor.fg(12),
+    Level.warning: AnsiColor.fg(208),
+    Level.error: AnsiColor.fg(196),
+    Level.wtf: AnsiColor.fg(199),
+  };
+
+  final bool printTime;
+  final bool colors;
+
+  ColorPrinter({this.printTime = true, this.colors = true});
+
+  @override
+  List<String> log(LogEvent event) {
+    var color = levelColors[event.level]!;
+    var messageStr = color(_stringifyMessage(event.message));
+    var errorStr = event.error != null ? color('  ERROR: ${event.error}') : '';
+    var timeStr = printTime ? color('TIME: ${DateTime.now().toIso8601String()}') : '';
+    return ['${_labelFor(event.level)}  $timeStr  $messageStr  <<<ERROR>>>:$errorStr'];
+  }
+
+  String _labelFor(Level level) {
+    var prefix = levelPrefixes[level]!;
+    var color = levelColors[level]!;
+
+    return colors ? color(prefix) : prefix;
+  }
+
+  String _stringifyMessage(dynamic message) {
+    if (message is Map || message is Iterable) {
+      var encoder = JsonEncoder.withIndent(null);
+      return encoder.convert(message);
+    } else {
+      return message.toString();
+    }
   }
 }
