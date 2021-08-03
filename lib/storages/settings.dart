f20c21022938341e4af9a08dbff1379ba2ee3524
guo <jiangzhiguo1992@163.com>
2021-08-03 17:53:47 +0800

    update localStorage keys

--- lib/storages/settings.dart
+++ lib/storages/settings.dart
@@ -5,12 +5,12 @@ import '../helpers/local_storage.dart';
 class SettingsStorage {
   static const String SETTINGS_KEY = 'SETTINGS';
   static const String LOCALE_KEY = 'locale';
+  static const String SEED_RPC_SERVERS_KEY = 'seed_rpc_servers'; // not support 'NKN_RPC_NODE_LIST'
   static const String NOTIFICATION_TYPE_KEY = 'notification_type'; // not support 'local_notification_type'
   static const String BIOMETRICS_AUTHENTICATION = 'biometrics_authentication'; // not support 'auth'
 
-  static const String SEED_RPC_SERVERS_KEY = 'seed_rpc_servers'; // not support 'NKN_RPC_NODE_LIST'
-
   static const String CHAT_TIP_STATUS = 'chat_tip_status'; // not support 'WALLET_TIP_STATUS'
+  static const String CHAT_TIP_NOTIFICATION = 'chat_tip_notification'; // not support 'NKN_MESSAGE_NOTIFICATION_ALERT'
 
   final LocalStorage _localStorage = LocalStorage();
 
@@ -51,5 +51,16 @@ class SettingsStorage {
     }
     results = LinkedHashSet<String>.from(results).toList();
     return results;
+  }
+
+  Future<bool> isNeedTipNotificationOpen(String? targetId) async {
+    if (targetId == null || targetId.isEmpty) return false;
+    var result = await _localStorage.get('$CHAT_TIP_NOTIFICATION:$targetId');
+    return result?.toString() != "1";
+  }
+
+  Future setNeedTipNotificationOpen(String? targetId) async {
+    if (targetId == null || targetId.isEmpty) return;
+    await _localStorage.set('$CHAT_TIP_NOTIFICATION:$targetId', "1");
   }
 }
