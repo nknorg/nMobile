8427784f14d0d2a1895b4725191f0ed1da2ff7a3
guo <jiangzhiguo1992@163.com>
2021-08-10 11:51:07 +0800

    update qrImage share

--- lib/screens/wallet/receive.dart
+++ lib/screens/wallet/receive.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -8,6 +10,7 @@ import 'package:nmobile/common/global.dart';
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/components/base/stateful.dart';
 import 'package:nmobile/components/button/button.dart';
+import 'package:nmobile/components/dialog/loading.dart';
 import 'package:nmobile/components/layout/header.dart';
 import 'package:nmobile/components/layout/layout.dart';
 import 'package:nmobile/components/text/label.dart';
@@ -16,8 +19,10 @@ import 'package:nmobile/generated/l10n.dart';
 import 'package:nmobile/schema/wallet.dart';
 import 'package:nmobile/utils/asset.dart';
 import 'package:nmobile/utils/logger.dart';
+import 'package:nmobile/utils/path.dart';
 import 'package:nmobile/utils/utils.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:share_plus/share_plus.dart';
 
 class WalletReceiveScreen extends BaseStateFulWidget {
   static const String routeName = '/wallet/receive';
@@ -58,27 +63,112 @@ class _WalletReceiveScreenState extends BaseStateFulWidgetState<WalletReceiveScr
   Widget build(BuildContext context) {
     S _localizations = S.of(context);
 
+    RepaintBoundary repaintBoundary = RepaintBoundary(
+      key: globalKey,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 24, left: 20, right: 20),
+            child: Material(
+              color: application.theme.backgroundColor2,
+              borderRadius: BorderRadius.all(Radius.circular(8)),
+              elevation: 0,
+              child: InkWell(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                onTap: () {
+                  copyText(_wallet.address, context: context);
+                },
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                  // decoration: BoxDecoration(
+                  //   borderRadius: BorderRadius.all(Radius.circular(8)),
+                  //   color: application.theme.backgroundColor2,
+                  // ),
+                  child: Column(
+                    children: <Widget>[
+                      Label(
+                        _localizations.wallet_address,
+                        type: LabelType.h4,
+                        textAlign: TextAlign.start,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                        child: Label(
+                          _wallet.address,
+                          type: LabelType.bodyRegular,
+                          textAlign: TextAlign.start,
+                          maxLines: 10,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Asset.iconSvg(
+                              'copy',
+                              width: 24,
+                              color: application.theme.primaryColor,
+                            ),
+                          ),
+                          Label(
+                            _localizations.copy_to_clipboard,
+                            color: application.theme.primaryColor,
+                            type: LabelType.h4,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(top: 24),
+            child: QrImage(
+              data: _wallet.address,
+              version: QrVersions.auto,
+              size: Global.screenWidth() * 0.57,
+            ),
+          ),
+        ],
+      ),
+    );
+
     return Layout(
       headerColor: application.theme.backgroundColor4,
       header: Header(
         title: (_localizations.receive + ' ' + _localizations.nkn),
         backgroundColor: application.theme.backgroundColor4,
-        // actions: [
-        //   IconButton(
-        //     icon: Asset.iconSVG(
-        //       'share',
-        //       width: 24,
-        //       color: application.theme.backgroundLightColor,
-        //     ),
-        //     onPressed: () async {
-        //       RenderRepaintBoundary boundary = globalKey.currentContext.findRenderObject();
-        //       var image = await boundary.toImage();
-        //       ByteData byteData = await image.toByteData(format: ImageByteFormat.png);
-        //       Uint8List pngBytes = byteData.buffer.asUint8List();
-        //       await Share.file('Recieve NKN', 'qrcode.png', pngBytes, 'image/png', text: wallet.address);
-        //     },
-        //   )
-        // ],
+        actions: [
+          IconButton(
+            icon: Asset.iconSvg(
+              'share',
+              width: 24,
+              color: application.theme.backgroundLightColor,
+            ),
+            onPressed: () async {
+              Loading.show();
+              // image
+              RenderRepaintBoundary? boundary = globalKey.currentContext?.findRenderObject() as RenderRepaintBoundary?;
+              var qrImg = await boundary?.toImage();
+              ByteData? imgData = await qrImg?.toByteData(format: ImageByteFormat.png);
+              Uint8List? imgBytes = imgData?.buffer.asUint8List();
+              String path = await Path.getCacheFile("cache", fileExt: "png");
+              if (imgBytes == null || imgBytes.isEmpty || path.isEmpty) {
+                Loading.dismiss();
+                return;
+              }
+              // file
+              File qrFile = await File(path).create();
+              qrFile = await qrFile.writeAsBytes(imgBytes);
+              Loading.dismiss();
+              // share
+              Share.shareFiles([qrFile.path]);
+            },
+          )
+        ],
       ),
       body: Column(
         children: <Widget>[
@@ -109,78 +199,7 @@ class _WalletReceiveScreenState extends BaseStateFulWidgetState<WalletReceiveScr
                         ),
                       ),
                       Divider(height: 3, indent: 20, endIndent: 20),
-                      RepaintBoundary(
-                        key: globalKey,
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(top: 24, left: 20, right: 20),
-                              child: Material(
-                                color: application.theme.backgroundColor2,
-                                borderRadius: BorderRadius.all(Radius.circular(8)),
-                                elevation: 0,
-                                child: InkWell(
-                                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                                  onTap: () {
-                                    copyText(_wallet.address, context: context);
-                                  },
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-                                    // decoration: BoxDecoration(
-                                    //   borderRadius: BorderRadius.all(Radius.circular(8)),
-                                    //   color: application.theme.backgroundColor2,
-                                    // ),
-                                    child: Column(
-                                      children: <Widget>[
-                                        Label(
-                                          _localizations.wallet_address,
-                                          type: LabelType.h4,
-                                          textAlign: TextAlign.start,
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.symmetric(vertical: 8),
-                                          child: Label(
-                                            _wallet.address,
-                                            type: LabelType.bodyRegular,
-                                            textAlign: TextAlign.start,
-                                            maxLines: 10,
-                                          ),
-                                        ),
-                                        Row(
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                          children: <Widget>[
-                                            Padding(
-                                              padding: const EdgeInsets.only(right: 8.0),
-                                              child: Asset.iconSvg(
-                                                'copy',
-                                                width: 24,
-                                                color: application.theme.primaryColor,
-                                              ),
-                                            ),
-                                            Label(
-                                              _localizations.copy_to_clipboard,
-                                              color: application.theme.primaryColor,
-                                              type: LabelType.h4,
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: const EdgeInsets.only(top: 24),
-                              child: QrImage(
-                                data: _wallet.address,
-                                version: QrVersions.auto,
-                                size: Global.screenWidth() * 0.57,
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
+                      repaintBoundary,
                     ],
                   );
                 },
