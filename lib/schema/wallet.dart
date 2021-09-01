f623275b5fbb5300a005beeecc4d35ec98ea4955
guo <jiangzhiguo1992@163.com>
2021-09-01 19:09:36 +0800

    update wallet schema

--- lib/schema/wallet.dart
+++ lib/schema/wallet.dart
@@ -4,46 +4,51 @@ class WalletType {
 }
 
 class WalletSchema {
-  String address;
   String type;
-
-  String? name;
+  String address;
+  String publicKey;
   double? balance = 0;
   double? balanceEth = 0;
 
-  // DEPRECATED:GG
-  // bool isBackedUp = false;
+  String? name;
+  bool isBackedUp = false;
 
   WalletSchema({
-    required this.address,
     required this.type,
-    this.name,
+    required this.address,
+    required this.publicKey,
     this.balance = 0,
     this.balanceEth = 0,
+    this.name,
+    this.isBackedUp = false,
   });
 
   static WalletSchema fromMap(Map<String, dynamic> map) {
     return WalletSchema(
-      address: map['address'] ?? "",
       type: map['type'] ?? "",
-      name: map['name'],
+      address: map['address'] ?? "",
+      publicKey: map['public_key'] ?? "",
       balance: map['balance'] ?? 0,
       balanceEth: map['balanceEth'] ?? 0,
+      name: map['name'],
+      isBackedUp: map['is_backed_up'],
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'address': address,
       'type': type,
-      'name': name,
+      'address': address,
+      'public_key': publicKey,
       'balance': balance ?? 0,
       'balanceEth': balanceEth ?? 0,
+      'name': name,
+      'is_backed_up': isBackedUp,
     };
   }
 
   @override
   String toString() {
-    return 'WalletSchema{type: $type, address: $address, name: $name, balance: $balance, balanceEth: $balanceEth}';
+    return 'WalletSchema{type: $type, address: $address, publicKey: $publicKey, balance: $balance, balanceEth: $balanceEth, name: $name, isBackedUp: $isBackedUp}';
   }
 }
