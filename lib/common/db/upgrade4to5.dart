1b250074f147a85a875bea5deac380ec2206c54a
guo <jiangzhiguo1992@163.com>
2021-09-18 19:26:54 +0800

    update package sqflite

--- lib/common/db/upgrade4to5.dart
+++ lib/common/db/upgrade4to5.dart
@@ -1,10 +1,24 @@
 import 'package:nmobile/storages/device_info.dart';
 import 'package:nmobile/storages/session.dart';
-import 'package:sqflite_sqlcipher/sqflite.dart';
+import 'package:sqflite/sqflite.dart';
 
 class Upgrade4to5 {
   static Future upgradeContact(Database db) async {
-    // TODO:GG db contact
+    // id (NULL) -> id (NOT NULL)
+    // TODO:GG address (TEXT) -> address (VARCHAR(200))
+    // TODO:GG type (TEXT) -> type (INT)
+    // TODO:GG created_time (INTEGER) -> create_at (BIGINT)
+    // TODO:GG updated_time (INTEGER) -> update_at (BIGINT)
+    // avatar (TEXT) -> avatar (TEXT)
+    // TODO:GG first_name (TEXT) -> first_name (VARCHAR(50))
+    // TODO:GG last_name (TEXT) -> last_name (VARCHAR(50))
+    // TODO:GG profile_version (TEXT) -> profile_version (VARCHAR(300))
+    // TODO:GG profile_expires_at (INTEGER) -> profile_expires_at (BIGINT)
+    // is_top (BOOLEAN) -> is_top (BOOLEAN)
+    // device_token (TEXT) -> device_token (TEXT)
+    // options (TEXT) -> options (TEXT)
+    // data (TEXT) -> data( TEXT)
+    // TODO:GG notification_open (BOOLEAN) -> options.notificationOpen
   }
 
   static Future createDeviceInfo(Database db) async {
