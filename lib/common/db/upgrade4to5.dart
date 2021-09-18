d470e8d07d2432d3a15625ac68d12f29eb0eab64
guo <jiangzhiguo1992@163.com>
2021-09-18 11:05:29 +0800

    add upgrade 4 to 5

--- lib/common/db/upgrade4to5.dart
+++ lib/common/db/upgrade4to5.dart
@@ -1,1 +1,31 @@
-class Upgrade4to5 {}
+import 'package:nmobile/storages/device_info.dart';
+import 'package:nmobile/storages/session.dart';
+import 'package:sqflite_sqlcipher/sqflite.dart';
+
+class Upgrade4to5 {
+  static Future upgradeContact(Database db) async {
+    // TODO:GG db contact
+  }
+
+  static Future createDeviceInfo(Database db) async {
+    await DeviceInfoStorage.create(db);
+  }
+
+  static Future upgradeTopic(Database db) async {
+    // TODO:GG db topic
+  }
+
+  static Future upgradeSubscriber(Database db) async {
+    // TODO:GG db subscriber
+  }
+
+  static Future upgradeMessages(Database db) async {
+    // TODO:GG db messages
+    // TODO:GG delete message(receipt) + read message(piece + contactOptions)
+  }
+
+  static Future createSession(Database db) async {
+    await SessionStorage.create(db);
+    // TODO:GG 取消息的最后一条，聚合成session，还有未读数等其他字段
+  }
+}
