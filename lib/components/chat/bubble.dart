d1b5381b23eaddae9ef954fec387b352eca3c90f
guo <jiangzhiguo1992@163.com>
2021-11-06 19:48:34 +0800

    update message notification check wait

--- lib/components/chat/bubble.dart
+++ lib/components/chat/bubble.dart
@@ -63,7 +63,6 @@ class _ChatBubbleState extends BaseStateFulWidgetState<ChatBubble> with Tag {
   StreamSubscription? _onPlayStateChangedSubscription;
   StreamSubscription? _onPlayPositionChangedSubscription;
 
-  // TODO:GG late
   late MessageSchema _message;
   ContactSchema? _contact;
 
@@ -363,7 +362,7 @@ class _ChatBubbleState extends BaseStateFulWidgetState<ChatBubble> with Tag {
               backgroundColor: application.theme.strongColor,
               onPressed: () {
                 widget.onResend?.call(_message.msgId);
-                if (Navigator.of(context).canPop()) Navigator.pop(this.context);
+                if (Navigator.of(this.context).canPop()) Navigator.pop(this.context);
               },
             ),
           );
