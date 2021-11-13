b8097a992e97778985f488c080e1c8e1b060b804
guo <jiangzhiguo1992@163.com>
2021-11-13 14:28:29 +0800

    update db topic + subscriber lock and offset + limit

--- lib/storages/subscriber.dart
+++ lib/storages/subscriber.dart
@@ -59,6 +59,8 @@ class SubscriberStorage with Tag {
               columns: ['*'],
               where: 'topic = ? AND chat_id = ?',
               whereArgs: [schema.topic, schema.clientAddress],
+              offset: 0,
+              limit: 1,
             );
             if (res != null && res.length > 0) {
               logger.w("$TAG - insert - duplicated - schema:$schema");
@@ -134,196 +136,204 @@ class SubscriberStorage with Tag {
   Future<SubscriberSchema?> query(int? subscriberId) async {
     if (db?.isOpen != true) return null;
     if (subscriberId == null || subscriberId == 0) return null;
-    return await _lock.synchronized(() async {
-      try {
-        List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['*'],
-            where: 'id = ?',
-            whereArgs: [subscriberId],
-          );
-        });
-        if (res != null && res.length > 0) {
-          SubscriberSchema? schema = SubscriberSchema.fromMap(res.first);
-          logger.v("$TAG - query - success - subscriberId:$subscriberId - schema:$schema");
-          return schema;
-        }
-        logger.v("$TAG - query - empty - subscriberId:$subscriberId");
-      } catch (e) {
-        handleError(e);
+    // return await _lock.synchronized(() async {
+    try {
+      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['*'],
+          where: 'id = ?',
+          whereArgs: [subscriberId],
+          offset: 0,
+          limit: 1,
+        );
+      });
+      if (res != null && res.length > 0) {
+        SubscriberSchema? schema = SubscriberSchema.fromMap(res.first);
+        logger.v("$TAG - query - success - subscriberId:$subscriberId - schema:$schema");
+        return schema;
       }
-      return null;
-    });
+      logger.v("$TAG - query - empty - subscriberId:$subscriberId");
+    } catch (e) {
+      handleError(e);
+    }
+    return null;
+    // });
   }
 
   Future<SubscriberSchema?> queryByTopicChatId(String? topic, String? chatId) async {
     if (db?.isOpen != true) return null;
     if (topic == null || topic.isEmpty || chatId == null || chatId.isEmpty) return null;
-    return await _lock.synchronized(() async {
-      try {
-        List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['*'],
-            where: 'topic = ? AND chat_id = ?',
-            whereArgs: [topic, chatId],
-          );
-        });
-        if (res != null && res.length > 0) {
-          SubscriberSchema? schema = SubscriberSchema.fromMap(res.first);
-          logger.v("$TAG - queryByTopicChatId - success - topic:$topic - chatId:$chatId - schema:$schema");
-          return schema;
-        }
-        logger.v("$TAG - queryByTopicChatId - empty -  - topic:$topic - chatId:$chatId");
-      } catch (e) {
-        handleError(e);
+    // return await _lock.synchronized(() async {
+    try {
+      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['*'],
+          where: 'topic = ? AND chat_id = ?',
+          whereArgs: [topic, chatId],
+          offset: 0,
+          limit: 1,
+        );
+      });
+      if (res != null && res.length > 0) {
+        SubscriberSchema? schema = SubscriberSchema.fromMap(res.first);
+        logger.v("$TAG - queryByTopicChatId - success - topic:$topic - chatId:$chatId - schema:$schema");
+        return schema;
       }
-      return null;
-    });
+      logger.v("$TAG - queryByTopicChatId - empty -  - topic:$topic - chatId:$chatId");
+    } catch (e) {
+      handleError(e);
+    }
+    return null;
+    // });
   }
 
-  Future<List<SubscriberSchema>> queryListByTopic(String? topic, {int? status, String? orderBy, int? limit, int? offset}) async {
+  Future<List<SubscriberSchema>> queryListByTopic(String? topic, {int? status, String? orderBy, int offset = 0, int limit = 20}) async {
     if (db?.isOpen != true) return [];
     if (topic == null || topic.isEmpty) return [];
-    return await _lock.synchronized(() async {
-      try {
-        List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['*'],
-            where: status != null ? 'topic = ? AND status = ?' : 'topic = ?',
-            whereArgs: status != null ? [topic, status] : [topic],
-            offset: offset ?? null,
-            limit: limit ?? null,
-            orderBy: orderBy ?? 'create_at ASC',
-          );
-        });
-        if (res == null || res.isEmpty) {
-          logger.v("$TAG - queryListByTopic - empty - topic:$topic - status:$status");
-          return [];
-        }
-        List<SubscriberSchema> results = <SubscriberSchema>[];
-        String logText = '';
-        res.forEach((map) {
-          logText += "\n      $map";
-          SubscriberSchema? subscriber = SubscriberSchema.fromMap(map);
-          if (subscriber != null) results.add(subscriber);
-        });
-        logger.v("$TAG - queryListByTopic - items:$logText");
-        return results;
-      } catch (e) {
-        handleError(e);
+    // return await _lock.synchronized(() async {
+    try {
+      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['*'],
+          where: status != null ? 'topic = ? AND status = ?' : 'topic = ?',
+          whereArgs: status != null ? [topic, status] : [topic],
+          offset: offset,
+          limit: limit,
+          orderBy: orderBy ?? 'create_at ASC',
+        );
+      });
+      if (res == null || res.isEmpty) {
+        logger.v("$TAG - queryListByTopic - empty - topic:$topic - status:$status");
+        return [];
       }
-      return [];
-    });
+      List<SubscriberSchema> results = <SubscriberSchema>[];
+      String logText = '';
+      res.forEach((map) {
+        logText += "\n      $map";
+        SubscriberSchema? subscriber = SubscriberSchema.fromMap(map);
+        if (subscriber != null) results.add(subscriber);
+      });
+      logger.v("$TAG - queryListByTopic - items:$logText");
+      return results;
+    } catch (e) {
+      handleError(e);
+    }
+    return [];
+    // });
   }
 
-  Future<List<SubscriberSchema>> queryListByTopicPerm(String? topic, int? permPage) async {
+  Future<List<SubscriberSchema>> queryListByTopicPerm(String? topic, int? permPage, int limit) async {
     if (db?.isOpen != true) return [];
     if (topic == null || topic.isEmpty || permPage == null) return [];
-    return await _lock.synchronized(() async {
-      try {
-        List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['*'],
-            where: 'topic = ? AND perm_page = ?',
-            whereArgs: [topic, permPage],
-          );
-        });
-        if (res == null || res.isEmpty) {
-          logger.v("$TAG - queryListByTopicPerm - empty - topic:$topic - permPage:$permPage");
-          return [];
-        }
-        List<SubscriberSchema> results = <SubscriberSchema>[];
-        String logText = '';
-        res.forEach((map) {
-          logText += "\n      $map";
-          SubscriberSchema? subscriber = SubscriberSchema.fromMap(map);
-          if (subscriber != null) results.add(subscriber);
-        });
-        logger.v("$TAG - queryListByTopicPerm - items:$logText");
-        return results;
-      } catch (e) {
-        handleError(e);
+    // return await _lock.synchronized(() async {
+    try {
+      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['*'],
+          where: 'topic = ? AND perm_page = ?',
+          whereArgs: [topic, permPage],
+          offset: 0,
+          limit: limit,
+        );
+      });
+      if (res == null || res.isEmpty) {
+        logger.v("$TAG - queryListByTopicPerm - empty - topic:$topic - permPage:$permPage");
+        return [];
       }
-      return [];
-    });
+      List<SubscriberSchema> results = <SubscriberSchema>[];
+      String logText = '';
+      res.forEach((map) {
+        logText += "\n      $map";
+        SubscriberSchema? subscriber = SubscriberSchema.fromMap(map);
+        if (subscriber != null) results.add(subscriber);
+      });
+      logger.v("$TAG - queryListByTopicPerm - items:$logText");
+      return results;
+    } catch (e) {
+      handleError(e);
+    }
+    return [];
+    // });
   }
 
   Future<int> queryCountByTopic(String? topic, {int? status}) async {
     if (db?.isOpen != true) return 0;
     if (topic == null || topic.isEmpty) return 0;
-    return await _lock.synchronized(() async {
-      try {
-        final res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['COUNT(id)'],
-            where: status != null ? 'topic = ? AND status = ?' : 'topic = ?',
-            whereArgs: status != null ? [topic, status] : [topic],
-          );
-        });
-        int? count = Sqflite.firstIntValue(res ?? <Map<String, dynamic>>[]);
-        logger.v("$TAG - queryCountByTopic - topic:$topic - count:$status");
-        return count ?? 0;
-      } catch (e) {
-        handleError(e);
-      }
-      return 0;
-    });
+    // return await _lock.synchronized(() async {
+    try {
+      final res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['COUNT(id)'],
+          where: status != null ? 'topic = ? AND status = ?' : 'topic = ?',
+          whereArgs: status != null ? [topic, status] : [topic],
+        );
+      });
+      int? count = Sqflite.firstIntValue(res ?? <Map<String, dynamic>>[]);
+      logger.v("$TAG - queryCountByTopic - topic:$topic - count:$status");
+      return count ?? 0;
+    } catch (e) {
+      handleError(e);
+    }
+    return 0;
+    // });
   }
 
   Future<int> queryCountByTopicPermPage(String? topic, int permPage, {int? status}) async {
     if (db?.isOpen != true) return 0;
     if (topic == null || topic.isEmpty) return 0;
-    return await _lock.synchronized(() async {
-      try {
-        final res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['COUNT(id)'],
-            where: status != null ? 'topic = ? AND perm_page = ? AND status = ?' : 'topic = ? AND perm_page = ?',
-            whereArgs: status != null ? [topic, permPage, status] : [topic, permPage],
-          );
-        });
-        int? count = Sqflite.firstIntValue(res ?? <Map<String, dynamic>>[]);
-        logger.v("$TAG - queryCountByTopicPermPage - topic:$topic - permPage:$permPage - count:$status");
-        return count ?? 0;
-      } catch (e) {
-        handleError(e);
-      }
-      return 0;
-    });
+    // return await _lock.synchronized(() async {
+    try {
+      final res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['COUNT(id)'],
+          where: status != null ? 'topic = ? AND perm_page = ? AND status = ?' : 'topic = ? AND perm_page = ?',
+          whereArgs: status != null ? [topic, permPage, status] : [topic, permPage],
+        );
+      });
+      int? count = Sqflite.firstIntValue(res ?? <Map<String, dynamic>>[]);
+      logger.v("$TAG - queryCountByTopicPermPage - topic:$topic - permPage:$permPage - count:$status");
+      return count ?? 0;
+    } catch (e) {
+      handleError(e);
+    }
+    return 0;
+    // });
   }
 
   Future<int> queryMaxPermPageByTopic(String? topic) async {
     if (db?.isOpen != true) return 0;
     if (topic == null || topic.isEmpty) return 0;
-    return await _lock.synchronized(() async {
-      try {
-        List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['*'],
-            where: 'topic = ?',
-            whereArgs: [topic],
-            orderBy: 'perm_page DESC',
-          );
-        });
-        if (res != null && res.length > 0) {
-          SubscriberSchema? schema = SubscriberSchema.fromMap(res.first);
-          logger.v("$TAG - queryMaxPermPageByTopic - success - topic:$topic - schema:$schema");
-          return schema?.permPage ?? 0;
-        }
-        logger.v("$TAG - queryMaxPermPageByTopic - empty - topic:$topic");
-        return 0;
-      } catch (e) {
-        handleError(e);
+    // return await _lock.synchronized(() async {
+    try {
+      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['*'],
+          where: 'topic = ?',
+          whereArgs: [topic],
+          orderBy: 'perm_page DESC',
+          offset: 0,
+          limit: 1,
+        );
+      });
+      if (res != null && res.length > 0) {
+        SubscriberSchema? schema = SubscriberSchema.fromMap(res.first);
+        logger.v("$TAG - queryMaxPermPageByTopic - success - topic:$topic - schema:$schema");
+        return schema?.permPage ?? 0;
       }
+      logger.v("$TAG - queryMaxPermPageByTopic - empty - topic:$topic");
       return 0;
-    });
+    } catch (e) {
+      handleError(e);
+    }
+    return 0;
+    // });
   }
 
   Future<bool> setStatus(int? subscriberId, int? status) async {
