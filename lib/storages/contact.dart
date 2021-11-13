2e87cd7f881900e0e8eed2e97ce2790f669636c0
guo <jiangzhiguo1992@163.com>
2021-11-13 13:57:19 +0800

    update db contact + deviceInfo lock and offset + limit

--- lib/storages/contact.dart
+++ lib/storages/contact.dart
@@ -67,6 +67,8 @@ class ContactStorage with Tag {
               columns: ['*'],
               where: 'address = ?',
               whereArgs: [schema.clientAddress],
+              offset: 0,
+              limit: 1,
             );
             if (res != null && res.length > 0) {
               logger.w("$TAG - insert - duplicated - schema:$schema");
@@ -118,127 +120,133 @@ class ContactStorage with Tag {
   Future<ContactSchema?> query(int? contactId) async {
     if (db?.isOpen != true) return null;
     if (contactId == null || contactId == 0) return null;
-    return await _lock.synchronized(() async {
-      try {
-        List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['*'],
-            where: 'id = ?',
-            whereArgs: [contactId],
-          );
-        });
-        if (res != null && res.length > 0) {
-          ContactSchema schema = await ContactSchema.fromMap(res.first);
-          logger.v("$TAG - query - success - contactId:$contactId - schema:$schema");
-          return schema;
-        }
-        logger.v("$TAG - query - empty - contactId:$contactId");
-      } catch (e) {
-        handleError(e);
+    // return await _lock.synchronized(() async {
+    try {
+      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['*'],
+          where: 'id = ?',
+          whereArgs: [contactId],
+          offset: 0,
+          limit: 1,
+        );
+      });
+      if (res != null && res.length > 0) {
+        ContactSchema schema = await ContactSchema.fromMap(res.first);
+        logger.v("$TAG - query - success - contactId:$contactId - schema:$schema");
+        return schema;
       }
-      return null;
-    });
+      logger.v("$TAG - query - empty - contactId:$contactId");
+    } catch (e) {
+      handleError(e);
+    }
+    return null;
+    // });
   }
 
   Future<ContactSchema?> queryByClientAddress(String? clientAddress) async {
     if (db?.isOpen != true) return null;
     if (clientAddress == null || clientAddress.isEmpty) return null;
-    return await _lock.synchronized(() async {
-      try {
-        List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['*'],
-            where: 'address = ?',
-            whereArgs: [clientAddress],
-          );
-        });
-        if (res != null && res.length > 0) {
-          ContactSchema schema = await ContactSchema.fromMap(res.first);
-          logger.v("$TAG - queryByClientAddress - success - address:$clientAddress - schema:$schema");
-          return schema;
-        }
-        logger.v("$TAG - queryByClientAddress - empty - address:$clientAddress");
-      } catch (e) {
-        handleError(e);
+    // return await _lock.synchronized(() async {
+    try {
+      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['*'],
+          where: 'address = ?',
+          whereArgs: [clientAddress],
+          offset: 0,
+          limit: 1,
+        );
+      });
+      if (res != null && res.length > 0) {
+        ContactSchema schema = await ContactSchema.fromMap(res.first);
+        logger.v("$TAG - queryByClientAddress - success - address:$clientAddress - schema:$schema");
+        return schema;
       }
-      return null;
-    });
+      logger.v("$TAG - queryByClientAddress - empty - address:$clientAddress");
+    } catch (e) {
+      handleError(e);
+    }
+    return null;
+    // });
   }
 
   Future<List<ContactSchema>> queryListByClientAddress(List<String>? clientAddressList) async {
     if (db?.isOpen != true) return [];
     if (clientAddressList == null || clientAddressList.isEmpty) return [];
-    return _lock.synchronized(() async {
-      try {
-        List? res = await db?.transaction((txn) {
-          Batch batch = txn.batch();
-          clientAddressList.forEach((clientAddress) {
-            if (clientAddress.isNotEmpty) {
-              batch.query(
-                tableName,
-                columns: ['*'],
-                where: 'address = ?',
-                whereArgs: [clientAddress],
-              );
-            }
-          });
-          return batch.commit();
-        });
-        if (res != null && res.length > 0) {
-          List<ContactSchema> schemaList = [];
-          for (var i = 0; i < res.length; i++) {
-            if (res[i] == null || res[i].isEmpty || res[i][0].isEmpty) continue;
-            Map<String, dynamic> map = res[i][0];
-            ContactSchema schema = await ContactSchema.fromMap(map);
-            schemaList.add(schema);
+    // return _lock.synchronized(() async {
+    try {
+      List? res = await db?.transaction((txn) {
+        Batch batch = txn.batch();
+        clientAddressList.forEach((clientAddress) {
+          if (clientAddress.isNotEmpty) {
+            batch.query(
+              tableName,
+              columns: ['*'],
+              where: 'address = ?',
+              whereArgs: [clientAddress],
+              offset: 0,
+              limit: 1,
+            );
           }
-          logger.v("$TAG - queryListByClientAddress - success - clientAddressList:$clientAddressList - schemaList:$schemaList");
-          return schemaList;
+        });
+        return batch.commit();
+      });
+      if (res != null && res.length > 0) {
+        List<ContactSchema> schemaList = [];
+        for (var i = 0; i < res.length; i++) {
+          if (res[i] == null || res[i].isEmpty || res[i][0].isEmpty) continue;
+          Map<String, dynamic> map = res[i][0];
+          ContactSchema schema = await ContactSchema.fromMap(map);
+          schemaList.add(schema);
         }
-        logger.v("$TAG - queryListByClientAddress - empty - clientAddressList:$clientAddressList");
-      } catch (e) {
-        handleError(e);
+        logger.v("$TAG - queryListByClientAddress - success - clientAddressList:$clientAddressList - schemaList:$schemaList");
+        return schemaList;
       }
-      return [];
-    });
+      logger.v("$TAG - queryListByClientAddress - empty - clientAddressList:$clientAddressList");
+    } catch (e) {
+      handleError(e);
+    }
+    return [];
+    // });
   }
 
-  Future<List<ContactSchema>> queryList({int? contactType, String? orderBy, int? limit, int? offset}) async {
+  Future<List<ContactSchema>> queryList({int? contactType, String? orderBy, int offset = 0, int limit = 20}) async {
     if (db?.isOpen != true) return [];
     orderBy = orderBy ?? (contactType == ContactType.friend ? 'create_at DESC' : 'update_at DESC');
-    return await _lock.synchronized(() async {
-      try {
-        List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-          return txn.query(
-            tableName,
-            columns: ['*'],
-            where: (contactType != null) ? 'type = ?' : null,
-            whereArgs: (contactType != null) ? [contactType] : null,
-            offset: offset ?? null,
-            limit: limit ?? null,
-            orderBy: orderBy,
-          );
-        });
-        if (res == null || res.isEmpty) {
-          logger.v("$TAG - queryList - empty - contactType:$contactType");
-          return [];
-        }
-        List<Future<ContactSchema>> futures = <Future<ContactSchema>>[];
-        String logText = '';
-        res.forEach((map) {
-          logText += "\n      $map";
-          futures.add(ContactSchema.fromMap(map));
-        });
-        List<ContactSchema> results = await Future.wait(futures);
-        logger.v("$TAG - queryList - items:$logText");
-        return results;
-      } catch (e) {
-        handleError(e);
+    // return await _lock.synchronized(() async {
+    try {
+      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
+        return txn.query(
+          tableName,
+          columns: ['*'],
+          where: (contactType != null) ? 'type = ?' : null,
+          whereArgs: (contactType != null) ? [contactType] : null,
+          offset: offset,
+          limit: limit,
+          orderBy: orderBy,
+        );
+      });
+      if (res == null || res.isEmpty) {
+        logger.v("$TAG - queryList - empty - contactType:$contactType");
+        return [];
       }
-      return [];
-    });
+      List<Future<ContactSchema>> futures = <Future<ContactSchema>>[];
+      String logText = '';
+      res.forEach((map) {
+        logText += "\n      $map";
+        futures.add(ContactSchema.fromMap(map));
+      });
+      List<ContactSchema> results = await Future.wait(futures);
+      logger.v("$TAG - queryList - items:$logText");
+      return results;
+    } catch (e) {
+      handleError(e);
+    }
+    return [];
+    // });
   }
 
   // Future<int> queryCountByClientAddress(String? clientAddress) async {
