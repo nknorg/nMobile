3a1afeaee8b29680cba0a41050f9b3f95faeb706
guo <jiangzhiguo1992@163.com>
2021-11-26 16:26:22 +0800

    update top_sub

--- lib/common/topic/top_sub.dart
+++ lib/common/topic/top_sub.dart
@@ -5,11 +5,100 @@ import 'package:nmobile/helpers/error.dart';
 import 'package:nmobile/utils/utils.dart';
 
 class TopSub {
-  /// ***********************************************************************************************************
-  /// *********************************************** subscribe *************************************************
-  /// ***********************************************************************************************************
+  // TODO:GG mean? subscriber = "identifier.publickey"
+  static Future<Map<String, dynamic>> getSubscription(String? topic, String? subscriber, {int tryCount = 0}) async {
+    if (topic == null || topic.isEmpty || subscriber == null || subscriber.isEmpty) return Map();
+    Map<String, dynamic>? results;
+    try {
+      if (clientCommon.isClientCreated && !clientCommon.clientClosing) {
+        results = await clientCommon.client?.getSubscription(
+          topic: genTopicHash(topic),
+          subscriber: subscriber,
+        );
+      }
+      if ((results == null) || results.isEmpty) {
+        List<String> seedRpcList = await Global.getSeedRpcList(null);
+        results = await Wallet.getSubscription(
+          genTopicHash(topic),
+          subscriber,
+          config: RpcConfig(seedRPCServerAddr: seedRpcList),
+        );
+      }
+    } catch (e) {
+      handleError(e);
+    }
+    if ((results == null) || results.isEmpty) {
+      if (tryCount < 2) {
+        await Future.delayed(Duration(seconds: 1));
+        return getSubscription(topic, subscriber, tryCount: ++tryCount);
+      } else {
+        results = Map();
+      }
+    }
+    return results;
+  }
 
-  static Future<int> getSubscribersCount(String? topic) async {
+  static Future<Map<String, dynamic>> getSubscribers(
+    String? topic, {
+    int offset = 0,
+    int limit = 10000,
+    bool meta = false,
+    bool txPool = true,
+    // Uint8List? subscriberHashPrefix,
+    int tryCount = 0,
+  }) async {
+    if (topic == null || topic.isEmpty) return Map();
+    Map<String, dynamic>? results;
+    try {
+      bool loop = true;
+      if (clientCommon.isClientCreated && !clientCommon.clientClosing) {
+        while (loop) {
+          Map<String, dynamic>? result = await clientCommon.client?.getSubscribers(
+            topic: genTopicHash(topic),
+            offset: offset,
+            limit: limit,
+            meta: meta,
+            txPool: txPool,
+            // subscriberHashPrefix: subscriberHashPrefix,
+          );
+          if ((result == null) || result.isEmpty) {
+            List<String> seedRpcList = await Global.getSeedRpcList(null);
+            result = await Wallet.getSubscribers(
+              topic: genTopicHash(topic),
+              offset: offset,
+              limit: limit,
+              meta: meta,
+              txPool: txPool,
+              // subscriberHashPrefix: subscriberHashPrefix,
+              config: RpcConfig(seedRPCServerAddr: seedRpcList),
+            );
+          }
+          if (result != null) {
+            if (results == null) {
+              results = result;
+            } else {
+              results.addAll(result);
+            }
+          }
+          loop = (result?.length ?? 0) >= limit;
+          offset += limit;
+        }
+      }
+    } catch (e) {
+      handleError(e);
+    }
+    if (results == null) {
+      if (tryCount < 2) {
+        await Future.delayed(Duration(seconds: 1));
+        return getSubscribers(topic, offset: 0, limit: limit, meta: meta, txPool: txPool, tryCount: ++tryCount);
+      } else {
+        results = Map();
+      }
+    }
+    return results;
+  }
+
+  static Future<int> getSubscribersCount(String? topic, {int tryCount = 0}) async {
     if (topic == null || topic.isEmpty) return 0;
     int? count;
     try {
@@ -30,40 +119,14 @@ class TopSub {
     } catch (e) {
       handleError(e);
     }
-    return count ?? 0;
-  }
-
-  static Future<Map<String, dynamic>> getSubscribers(
-    String? topic, {
-    int offset = 0,
-    int limit = 10000,
-    bool meta = false,
-    bool txPool = true,
-    // Uint8List? subscriberHashPrefix,
-  }) async {
-    if (topic == null || topic.isEmpty) return Map();
-    Map<String, dynamic> results = Map();
-    try {
-      bool loop = true;
-      while (loop) {
-        Map<String, dynamic>? result = await clientCommon.client?.getSubscribers(
-          topic: genTopicHash(topic),
-          offset: offset,
-          limit: limit,
-          meta: meta,
-          txPool: txPool,
-          // subscriberHashPrefix: subscriberHashPrefix,
-        );
-        results.addAll(result ?? Map());
-        offset += limit;
-        loop = result?.isNotEmpty == true;
+    if (count == null) {
+      if (tryCount < 2) {
+        await Future.delayed(Duration(seconds: 1));
+        return getSubscribersCount(topic, tryCount: ++tryCount);
+      } else {
+        count = 0;
       }
-    } catch (e) {
-      handleError(e);
-      return results;
     }
-    return results;
+    return count;
   }
-
-// TODO:GG _clientGetSubscription
 }
