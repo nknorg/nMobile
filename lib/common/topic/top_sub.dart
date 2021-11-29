405dba7f364d52691b6489bf1aa57588b67608bb
guo <jiangzhiguo1992@163.com>
2021-11-29 19:32:58 +0800

    update top_sub

--- lib/common/topic/top_sub.dart
+++ lib/common/topic/top_sub.dart
@@ -1,11 +1,136 @@
+import 'dart:convert';
+
 import 'package:nkn_sdk_flutter/wallet.dart';
 import 'package:nmobile/common/global.dart';
 import 'package:nmobile/common/locator.dart';
+import 'package:nmobile/components/tip/toast.dart';
 import 'package:nmobile/helpers/error.dart';
+import 'package:nmobile/utils/logger.dart';
 import 'package:nmobile/utils/utils.dart';
 
 class TopSub {
+  static Future<bool> subscribeWithPermission(
+    String? topic, {
+    double fee = 0,
+    int? permissionPage,
+    Map<String, dynamic>? meta,
+    int? nonce,
+    bool toast = false,
+    int tryCount = 1,
+    String? clientAddress,
+    int? oldStatus,
+    int? newStatus,
+  }) async {
+    String identifier = permissionPage != null ? '__${permissionPage}__.__permission__' : "";
+    String metaString = (meta?.isNotEmpty == true) ? jsonEncode(meta) : "";
+    List<bool> results = await _subscribe(topic, fee: fee, identifier: identifier, meta: metaString, nonce: nonce, toast: toast, tryCount: tryCount);
+    subscriberCommon.queryByTopicChatId(topic, clientAddress).then((value) async {
+      if (value == null || oldStatus == null || newStatus == null) return;
+      if (results[0]) {
+        Map<String, dynamic> newData = value.newDataByAppendStatus(newStatus, true);
+        logger.i("TopSub - _clientSubscribe - add permission try - topic:$topic - clientAddress:$clientAddress - newData:$newData - nonce:$nonce - identifier:$identifier - metaString:$metaString");
+        subscriberCommon.setData(value.id, newData); // await
+      } else if (!results[1]) {
+        Map<String, dynamic> newData = value.newDataByAppendStatus(newStatus, false);
+        logger.w("TopSub - _clientSubscribe - cancel permission try - topic:$topic - clientAddress:$clientAddress - newData:$newData - nonce:$nonce - identifier:$identifier - metaString:$metaString");
+        subscriberCommon.setData(value.id, newData).then((_) => subscriberCommon.setStatus(value.id, oldStatus, notify: true)); // await
+      }
+    }); // await
+    return results[0];
+  }
+
+  // TODO:GG identifier可以传马甲吗？
+  static Future<bool> subscribeWithJoin(
+    String? topic, {
+    double fee = 0,
+    String identifier = "",
+    int? nonce,
+    bool toast = false,
+    int tryCount = 0,
+  }) async {
+    List<bool> results = await _subscribe(topic, fee: fee, identifier: identifier, meta: "", nonce: nonce, toast: toast, tryCount: tryCount);
+    topicCommon.queryByTopic(topic).then((value) {
+      if (value == null) return;
+      if (results[0]) {
+        Map<String, dynamic> newData = value.newDataByAppendSubscribe(true, true);
+        logger.i("TopSub - subscribeWithJoin - add subscribe try - topic:$topic - newData:$newData - nonce:$nonce - identifier:$identifier");
+        topicCommon.setData(value.id, newData); // await
+      } else if (!results[1]) {
+        Map<String, dynamic> newData = value.newDataByAppendSubscribe(true, false);
+        logger.w("TopSub - subscribeWithJoin - cancel subscribe try - topic:$topic - newData:$newData - nonce:$nonce - identifier:$identifier");
+        topicCommon.setData(value.id, newData).then((_) => topicCommon.setJoined(value.id, false, notify: true)); // await
+      }
+    }); // await
+    return results[0];
+  }
+
+  static Future<List<bool>> _subscribe(
+    String? topic, {
+    double fee = 0,
+    String identifier = "",
+    String meta = "",
+    int? nonce,
+    bool toast = false,
+    int tryCount = 0,
+  }) async {
+    if (topic == null || topic.isEmpty) return [false, false];
+    bool isJoined = meta.isEmpty;
+    nonce = nonce ?? await Global.getNonce();
+
+    bool? success;
+    bool canTryTimer = true;
+    try {
+      if (clientCommon.isClientCreated && !clientCommon.clientClosing) {
+        String? topicHash = await clientCommon.client?.subscribe(
+          topic: genTopicHash(topic),
+          duration: Global.topicDefaultSubscribeHeight,
+          fee: fee.toString(),
+          identifier: identifier,
+          meta: meta,
+          nonce: nonce,
+        );
+        success = (topicHash != null) && (topicHash.isNotEmpty);
+      }
+    } catch (e) {
+      if (e.toString().contains("nonce is not continuous")) {
+        // can not append tx to txpool: nonce is not continuous
+        logger.w("TopSub - _subscribe - try over by nonce is not continuous - tryCount:$tryCount - topic:$topic - nonce:$nonce - identifier:$identifier - meta:$meta");
+        if (tryCount >= 2) {
+          if (toast && identifier.isEmpty) Toast.show(Global.locale((s) => s.something_went_wrong));
+          success = !isJoined; // permission action can add to try timer
+        } else {
+          nonce = await Global.getNonce(forceFetch: true);
+        }
+      } else if (e.toString().contains("doesn't exist")) {
+        logger.w("TopSub - _subscribe - topic doesn't exist - tryCount:$tryCount - topic:$topic - nonce:$nonce - identifier:$identifier - meta:$meta");
+        success = false;
+        canTryTimer = false;
+      } else if (e.toString().contains('duplicate subscription exist in block')) {
+        // can not append tx to txpool: duplicate subscription exist in block
+        logger.i("TopSub - _subscribe - block duplicated - tryCount:$tryCount - topic:$topic - nonce:$nonce - identifier:$identifier - meta:$meta");
+        if (toast && identifier.isEmpty) Toast.show(Global.locale((s) => s.request_processed));
+        nonce = await Global.refreshNonce();
+        success = !isJoined; // permission action can add to try timer
+      } else {
+        nonce = await Global.getNonce(forceFetch: true);
+        if (tryCount >= 2) {
+          success = false;
+          handleError(e);
+        }
+      }
+    }
+    if (success == null) {
+      if (tryCount < 2) {
+        return _subscribe(topic, fee: fee, identifier: identifier, meta: meta, nonce: nonce, toast: toast, tryCount: ++tryCount);
+      } else {
+        success = !isJoined; // permission action can add to try timer
+      }
+    }
+    return [success, canTryTimer];
+  }
+
   // TODO:GG mean? subscriber = "identifier.publickey"
+  // TODO:GG 返回的都是啥
   static Future<Map<String, dynamic>> getSubscription(String? topic, String? subscriber, {int tryCount = 0}) async {
     if (topic == null || topic.isEmpty || subscriber == null || subscriber.isEmpty) return Map();
     Map<String, dynamic>? results;
