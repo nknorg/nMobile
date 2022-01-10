f87f9200d27427ac421494ba738cd12ded59c1f8
guo <jiangzhiguo1992@163.com>
2022-01-10 18:55:23 +0800

    add client create params num_clients

--- plugins/nkn-sdk-flutter/lib/client.dart
+++ plugins/nkn-sdk-flutter/lib/client.dart
@@ -122,11 +122,12 @@ class Client {
   /// config value will be used. If config is nil, the default client config will
   /// be used.
   static Future<Client> create(Uint8List seed,
-      {String identifier = '', ClientConfig? config}) async {
+      {String identifier = '', int? numSubClients, ClientConfig? config}) async {
     try {
       final Map resp = await _methodChannel.invokeMethod('create', {
         'identifier': identifier,
         'seed': seed,
+        'numSubClients': numSubClients,
         'seedRpc': config?.seedRPCServerAddr?.isNotEmpty == true
             ? config?.seedRPCServerAddr
             : null,
