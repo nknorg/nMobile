b8cd853a8ce58fa773aec767a81ce497c0d06c4d
guo <jiangzhiguo1992@163.com>
2022-03-25 17:32:58 +0800

    fixed fee set in schema

--- lib/storages/subscriber.dart
+++ lib/storages/subscriber.dart
@@ -402,7 +402,6 @@ class SubscriberStorage with Tag {
             tableName,
             {
               'data': (newData?.isNotEmpty == true) ? jsonEncode(newData) : null,
-              'update_at': DateTime.now().millisecondsSinceEpoch,
             },
             where: 'id = ?',
             whereArgs: [subscriberId],
