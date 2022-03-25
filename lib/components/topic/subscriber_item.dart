b81df2708f183b70780ec056186dcfafd7d17c8f
guo <jiangzhiguo1992@163.com>
2022-03-25 17:59:24 +0800

    add subscribe quickly options

--- lib/components/topic/subscriber_item.dart
+++ lib/components/topic/subscriber_item.dart
@@ -7,6 +7,7 @@ import 'package:nmobile/components/base/stateful.dart';
 import 'package:nmobile/components/button/button.dart';
 import 'package:nmobile/components/contact/avatar.dart';
 import 'package:nmobile/components/contact/item.dart';
+import 'package:nmobile/components/dialog/bottom.dart';
 import 'package:nmobile/components/dialog/modal.dart';
 import 'package:nmobile/components/text/label.dart';
 import 'package:nmobile/schema/contact.dart';
@@ -263,11 +264,14 @@ class _SubscriberItemState extends BaseStateFulWidgetState<SubscriberItem> {
                 backgroundColor: application.theme.strongColor,
                 onPressed: () async {
                   if (Navigator.of(this.context).canPop()) Navigator.pop(this.context);
+                  double? fee = await BottomDialog.of(this.context).showSubscribeFee();
+                  if (fee == null) return;
                   await topicCommon.kick(
                     topic.topic,
                     topic.isPrivate,
                     topic.isOwner(clientCommon.address),
                     subscriber.clientAddress,
+                    fee: fee,
                     toast: true,
                   );
                 },
@@ -283,11 +287,14 @@ class _SubscriberItemState extends BaseStateFulWidgetState<SubscriberItem> {
               ),
             );
           } else {
+            double? fee = await BottomDialog.of(this.context).showSubscribeFee();
+            if (fee == null) return;
             await topicCommon.invitee(
               topic.topic,
               topic.isPrivate,
               topic.isOwner(clientCommon.address),
               subscriber.clientAddress,
+              fee: fee,
               toast: true,
               sendMsg: true,
             );
