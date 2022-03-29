c824c70141b13d396ca404398d72a704c0d679b9
guo <jiangzhiguo1992@163.com>
2022-03-29 17:24:32 +0800

    rename storage key name

--- lib/storages/settings.dart
+++ lib/storages/settings.dart
@@ -18,8 +18,8 @@ class SettingsStorage {
   static const String DATABASE_CLEAN_PWD_ON_IOS_14 = "database_clean_pwd_on_ios_14";
   static const String DATABASE_RESET_PWD_ON_IOS_16 = "database_reset_pwd_on_ios_16";
 
-  static const String DEFAULT_TOPIC_SUBSCRIBE_FEE = "default_topic_subscribe_fee";
-  static const String DEFAULT_TOPIC_SUBSCRIBE_SPEED_ENABLE = "default_topic_subscribe_speed_enable";
+  static const String DEFAULT_FEE = "default_fee";
+  static const String DEFAULT_TOPIC_RESUBSCRIBE_SPEED_ENABLE = "default_topic_resubscribe_speed_enable";
 
   static final LocalStorage _localStorage = LocalStorage();
 
