b4cb3d788156a9e828954154d63156c584f5c487
guo <jiangzhiguo1992@163.com>
2022-05-10 17:42:10 +0800

    add video screen

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -3,11 +3,13 @@ import 'package:nmobile/routes/routes.dart';
 import 'package:nmobile/screens/common/photo.dart';
 import 'package:nmobile/screens/common/scanner.dart';
 import 'package:nmobile/screens/common/select.dart';
+import 'package:nmobile/screens/common/video.dart';
 
 Map<String, WidgetBuilder> _routes = {
   ScannerScreen.routeName: (BuildContext context) => ScannerScreen(),
   SelectScreen.routeName: (BuildContext context, {arguments}) => SelectScreen(arguments: arguments),
   PhotoScreen.routeName: (BuildContext context, {arguments}) => PhotoScreen(arguments: arguments),
+  VideoScreen.routeName: (BuildContext context, {arguments}) => VideoScreen(arguments: arguments),
 };
 
 init() {
