308025236167d760d49e2b333f5812c89e684424
guo <jiangzhiguo1992@163.com>
2022-07-07 19:21:06 +0800

    upgrade some pub

--- lib/utils/util.dart
+++ lib/utils/util.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:nmobile/common/global.dart';
 import 'package:nmobile/components/tip/toast.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
 import 'logger.dart';
 
@@ -16,9 +17,24 @@ class Util {
   static void launchUrl(String? url) async {
     if (url == null || url.isEmpty) return;
     try {
-      await launch(url, forceSafariVC: false);
+      final Uri _uri = Uri.parse(url);
+      await UrlLauncher.launchUrl(_uri);
     } catch (e) {
       logger.w("Util - launchUrl ---> $e");
+    }
+  }
+
+  static Future launchFile(String? filePath) async {
+    if (filePath == null || filePath.isEmpty) return;
+    try {
+      final Uri _uri = Uri.file(filePath);
+      if (await File(_uri.toFilePath()).exists()) {
+        await UrlLauncher.launchUrl(_uri);
+      } else {
+        logger.w("Util - launchFile ---> file not exist");
+      }
+    } catch (e) {
+      logger.w("Util - launchFile ---> $e");
     }
   }
 
