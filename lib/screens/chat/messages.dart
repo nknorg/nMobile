2b3a94e7288b590b5a9a5f513c1b5eebcbc81e1d
guo <jiangzhiguo1992@163.com>
2022-07-21 18:31:41 +0800

    update piece max size

--- lib/screens/chat/messages.dart
+++ lib/screens/chat/messages.dart
@@ -653,7 +653,7 @@ class _ChatMessagesScreenState extends BaseStateFulWidgetState<ChatMessagesScree
                     }
                     int size = await content.length();
                     logger.d("$TAG - onRecordTap - saveFileSize:${Format.flowSize(size.toDouble(), unitArr: ['B', 'KB', 'MB', 'GB'])}");
-                    if (size <= MessageSchema.piecesBestMaxSize) {
+                    if (size <= MessageSchema.piecesMaxSize) {
                       return chatOutCommon.sendAudio(_topic ?? _contact, content, durationMs / 1000); // await
                     } else {
                       return chatOutCommon.saveIpfs(_topic ?? _contact, {
@@ -694,9 +694,9 @@ class _ChatMessagesScreenState extends BaseStateFulWidgetState<ChatMessagesScree
                           if (path.isEmpty) continue;
                           // no message_type(video/file), and result no mime_type from file_picker
                           // so big_file and video+file go with type_ipfs
-                          if ((mimeType?.contains("image") == true) && (size <= MessageSchema.piecesBestMaxSize)) {
+                          if ((mimeType?.contains("image") == true) && (size <= MessageSchema.piecesMaxSize)) {
                             chatOutCommon.sendImage(_topic ?? _contact, File(path)); // await
-                          } else if ((mimeType?.contains("audio") == true) && (size <= MessageSchema.piecesBestMaxSize)) {
+                          } else if ((mimeType?.contains("audio") == true) && (size <= MessageSchema.piecesMaxSize)) {
                             chatOutCommon.sendAudio(_topic ?? _contact, File(path), durationS); // await
                           } else {
                             chatOutCommon.saveIpfs(_topic ?? _contact, result); // await
