df91b60e59ca554efc6cdd387c6dba828d1c523a
guo <jiangzhiguo1992@163.com>
2022-09-09 16:41:17 +0800

    refactor private_group_storage

--- lib/storages/private_group.dart
+++ lib/storages/private_group.dart
@@ -1,29 +1,35 @@
+import 'dart:convert';
+
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/helpers/error.dart';
 import 'package:nmobile/schema/private_group.dart';
 import 'package:nmobile/utils/logger.dart';
+import 'package:nmobile/utils/parallel_queue.dart';
 import 'package:sqflite_sqlcipher/sqflite.dart';
-import 'package:synchronized/synchronized.dart';
 
-// TODO:GG PG check
 class PrivateGroupStorage with Tag {
   static String get tableName => 'PrivateGroup';
 
+  static PrivateGroupStorage instance = PrivateGroupStorage();
+
   Database? get db => dbCommon.database;
-  Lock _lock = new Lock();
+
+  ParallelQueue _queue = ParallelQueue("storage_private_group", onLog: (log, error) => error ? logger.w(log) : null);
 
   static String createSQL = '''
       CREATE TABLE `$tableName` (
         `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        `group_id` VARCHAR(100),
+        `group_id` VARCHAR(200),
         `name` VARCHAR(200),
-        `avatar` TEXT,
+        `type` INT,
         `version` TEXT,
         `count` INT,
-        `is_top` BOOLEAN DEFAULT 0,
-        `options` TEXT,
         `create_at` BIGINT,
-        `update_at` BIGINT
+        `update_at` BIGINT,
+        `is_top` BOOLEAN DEFAULT 0,
+        `avatar` TEXT,
+        `options` TEXT,
+        `data` TEXT
       )''';
 
   static create(Database db) async {
@@ -33,32 +39,55 @@ class PrivateGroupStorage with Tag {
     // index
     await db.execute('CREATE UNIQUE INDEX `index_unique_private_group_group_id` ON `$tableName` (`group_id`)');
     await db.execute('CREATE INDEX `index_private_group_name` ON `$tableName` (`name`)');
-    await db.execute('CREATE INDEX `index_private_group_version` ON `$tableName` (`version`)');
-    await db.execute('CREATE INDEX `index_private_group_create_at` ON `$tableName` (`create_at`)');
-    await db.execute('CREATE INDEX `index_private_group_update_at` ON `$tableName` (`update_at`)');
   }
 
-  Future<PrivateGroupSchema?> insert(PrivateGroupSchema schema) async {
+  Future<PrivateGroupSchema?> insert(PrivateGroupSchema? schema, {bool checkDuplicated = true}) async {
+    if (db?.isOpen != true) return null;
+    if (schema == null || schema.groupId.isEmpty) return null;
     Map<String, dynamic> entity = schema.toMap();
-    try {
-      int? id = await db?.insert(tableName, entity);
-
-      if (id != null && id > 0) {
-        schema.id = id;
-        logger.v("$TAG - insert - success - schema:$schema");
-        return schema;
-      }
-      logger.w("$TAG - insert - empty - schema:$schema");
-    } catch (e, st) {
-      if (e.toString().contains('UNIQUE constraint failed: PrivateGroup.group_id')) {
-      } else {
+    return await _queue.add(() async {
+      try {
+        int? id;
+        if (!checkDuplicated) {
+          id = await db?.transaction((txn) {
+            return txn.insert(tableName, entity);
+          });
+        } else {
+          id = await db?.transaction((txn) async {
+            List<Map<String, dynamic>> res = await txn.query(
+              tableName,
+              columns: ['*'],
+              where: 'group_id = ?',
+              whereArgs: [schema.groupId],
+              offset: 0,
+              limit: 1,
+            );
+            if (res != null && res.length > 0) {
+              logger.w("$TAG - insert - duplicated - schema:$schema");
+              return null;
+            } else {
+              return await txn.insert(tableName, entity);
+            }
+          });
+        }
+        if (id != null) {
+          PrivateGroupSchema schema = PrivateGroupSchema.fromMap(entity);
+          schema.id = id;
+          logger.v("$TAG - insert - success - schema:$schema");
+          return schema;
+        } else {
+          logger.i("$TAG - insert - exists - schema:$schema");
+        }
+      } catch (e, st) {
         handleError(e, st);
       }
-    }
-    return null;
+      return null;
+    });
   }
 
-  Future<PrivateGroupSchema?> query(String groupId) async {
+  Future<PrivateGroupSchema?> query(String? groupId) async {
+    if (db?.isOpen != true) return null;
+    if (groupId == null || groupId.isEmpty) return null;
     try {
       List<Map<String, dynamic>>? res = await db?.transaction((txn) {
         return txn.query(
@@ -71,7 +100,7 @@ class PrivateGroupStorage with Tag {
         );
       });
       if (res != null && res.length > 0) {
-        PrivateGroupSchema? schema = PrivateGroupSchema.fromMap(res.first);
+        PrivateGroupSchema schema = PrivateGroupSchema.fromMap(res.first);
         logger.v("$TAG - query - success - groupId:$groupId - schema:$schema");
         return schema;
       }
@@ -82,73 +111,87 @@ class PrivateGroupStorage with Tag {
     return null;
   }
 
-  Future<bool> setAvatar(String groupId, String? avatarLocalPath) async {
+  Future<bool> setAvatar(String? groupId, String? avatarLocalPath) async {
     if (db?.isOpen != true) return false;
-    return await _lock.synchronized(() async {
-      try {
-        int? count = await db?.transaction((txn) {
-          return txn.update(
-            tableName,
-            {
-              'avatar': avatarLocalPath,
-              'update_at': DateTime.now().millisecondsSinceEpoch,
-            },
-            where: 'group_id = ?',
-            whereArgs: [groupId],
-          );
-        });
-        if (count != null && count > 0) {
-          logger.v("$TAG - setAvatar - success - groupId:$groupId - avatarLocalPath:$avatarLocalPath");
-          return true;
-        }
-        logger.w("$TAG - setAvatar - fail - groupId:$groupId - avatarLocalPath:$avatarLocalPath");
-      } catch (e, st) {
-        handleError(e, st);
-      }
-      return false;
-    });
+    if (groupId == null || groupId.isEmpty) return false;
+    return await _queue.add(() async {
+          try {
+            int? count = await db?.transaction((txn) {
+              return txn.update(
+                tableName,
+                {
+                  'avatar': avatarLocalPath,
+                  'update_at': DateTime.now().millisecondsSinceEpoch,
+                },
+                where: 'group_id = ?',
+                whereArgs: [groupId],
+              );
+            });
+            if (count != null && count > 0) {
+              logger.v("$TAG - setAvatar - success - groupId:$groupId - avatarLocalPath:$avatarLocalPath");
+              return true;
+            }
+            logger.w("$TAG - setAvatar - fail - groupId:$groupId - avatarLocalPath:$avatarLocalPath");
+          } catch (e, st) {
+            handleError(e, st);
+          }
+          return false;
+        }) ??
+        false;
   }
 
-  Future<bool> update(String groupId, PrivateGroupSchema schema) async {
-    Map<String, dynamic> model = schema.toMap();
-    model.remove('id');
-    model.remove('create_at');
-    model['update_at'] = DateTime.now().millisecondsSinceEpoch;
-
-    model.removeWhere((key, value) => value == null);
-    try {
-      int? count = await db?.update(
-        tableName,
-        model,
-        where: 'group_id = ?',
-        whereArgs: [groupId],
-      );
-      return count != null && count > 0;
-    } catch (e, st) {
-      handleError(e, st);
-    }
-    return false;
+  Future<bool> updateVersionCount(String? groupId, String? version, int userCount) async {
+    if (db?.isOpen != true) return false;
+    if (groupId == null || groupId.isEmpty) return false;
+    return await _queue.add(() async {
+          try {
+            int? count = await db?.transaction((txn) {
+              return txn.update(
+                tableName,
+                {
+                  'version': version,
+                  'count': userCount,
+                  'update_at': DateTime.now().millisecondsSinceEpoch,
+                },
+                where: 'group_id = ?',
+                whereArgs: [groupId],
+              );
+            });
+            if (count != null && count > 0) {
+              logger.v("$TAG - updateVersionCount - success - groupId:$groupId - count:$count");
+              return true;
+            }
+            logger.w("$TAG - updateVersionCount - fail - groupId:$groupId - count:$count");
+          } catch (e, st) {
+            handleError(e, st);
+          }
+          return false;
+        }) ??
+        false;
   }
 
-  Future<int?> delete() async {
-    if (db?.isOpen != true) return null;
-
-    try {
-      int? res = await db?.transaction((txn) {
-        return txn.delete(tableName);
-      });
-      return res;
-    } catch (e, st) {
-      handleError(e, st);
-    }
-    return null;
-  }
-
-  Future<void> dropTable() async {
-    try {
-      await db?.execute('DROP TABLE $tableName');
-    } catch (e, st) {
-      handleError(e, st);
-    }
+  Future<bool> updateOptions(String? groupId, Map<String, dynamic>? options) async {
+    if (db?.isOpen != true) return false;
+    if (groupId == null || groupId.isEmpty) return false;
+    return await _queue.add(() async {
+          try {
+            int? count = await db?.transaction((txn) {
+              return txn.update(
+                tableName,
+                {
+                  'options': options != null ? jsonEncode(options) : null,
+                },
+                where: 'group_id = ?',
+                whereArgs: [groupId],
+              );
+            });
+            logger.v("$TAG - updateOptions - count:$count - groupId:$groupId - options:$options");
+            return (count ?? 0) > 0;
+          } catch (e, st) {
+            handleError(e, st);
+          }
+          return false;
+        }) ??
+        false;
   }
 }
