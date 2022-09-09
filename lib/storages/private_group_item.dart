fd53d0e666fed18af0ff7c77ba9faebddeefa19b
guo <jiangzhiguo1992@163.com>
2022-09-09 17:52:55 +0800

    refactor private_group_item_storage

--- lib/storages/private_group_item.dart
+++ lib/storages/private_group_item.dart
@@ -2,29 +2,32 @@ import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/helpers/error.dart';
 import 'package:nmobile/schema/private_group_item.dart';
 import 'package:nmobile/utils/logger.dart';
+import 'package:nmobile/utils/parallel_queue.dart';
 import 'package:sqflite_sqlcipher/sqflite.dart';
 
-// TODO:GG PG check
 class PrivateGroupItemStorage with Tag {
   static String get tableName => 'PrivateGroupList';
 
+  static PrivateGroupItemStorage instance = PrivateGroupItemStorage();
+
   Database? get db => dbCommon.database;
+
+  ParallelQueue _queue = ParallelQueue("storage_private_group_item", onLog: (log, error) => error ? logger.w(log) : null);
 
   static String createSQL = '''
       CREATE TABLE `$tableName` (
         `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        `group_id` VARCHAR(100),
-        `invitee` VARCHAR(100),
-        `inviter` VARCHAR(100),
-        `invitee_signature` VARCHAR(200),
-        `inviter_signature` VARCHAR(200),
+        `group_id` VARCHAR(200),
+        `permission` INT,
+        `expires_at` BIGINT,
+        `invitee` VARCHAR(200),
+        `inviter` VARCHAR(200),
+        `invitee_at` BIGINT,
+        `invited_at` BIGINT,
         `invitee_raw_data` TEXT,
         `inviter_raw_data` TEXT,
-        `invite_time` BIGINT,
-        `invited_time` BIGINT,
-        `expires_at` BIGINT,
-        `create_at` BIGINT,
-        `update_at` BIGINT
+        `invitee_signature` VARCHAR(200),
+        `inviter_signature` VARCHAR(200)
       )''';
 
   static create(Database db) async {
@@ -32,63 +35,61 @@ class PrivateGroupItemStorage with Tag {
     await db.execute(createSQL);
 
     // index
-    await db.execute('CREATE INDEX `index_private_group_list_invitee` ON `$tableName` (`invitee`)');
-    await db.execute('CREATE INDEX `index_private_group_list_inviter` ON `$tableName` (`inviter`)');
-    await db.execute('CREATE INDEX `index_private_group_list_invitee_signature` ON `$tableName` (`invitee_signature`)');
-    await db.execute('CREATE INDEX `index_private_group_list_inviter_signature` ON `$tableName` (`inviter_signature`)');
-    await db.execute('CREATE INDEX `index_private_group_list_invite_time` ON `$tableName` (`invite_time`)');
-    await db.execute('CREATE INDEX `index_private_group_list_invited_time` ON `$tableName` (`invited_time`)');
-    await db.execute('CREATE INDEX `index_private_group_list_expires_at` ON `$tableName` (`expires_at`)');
-    await db.execute('CREATE INDEX `index_private_group_list_create_at` ON `$tableName` (`create_at`)');
-    await db.execute('CREATE INDEX `index_private_group_list_update_at` ON `$tableName` (`update_at`)');
+    await db.execute('CREATE INDEX `index_private_group_item_group_id_invitee_expires_at` ON `$tableName` (`group_id`, `invitee`, `expires_at`)');
+    await db.execute('CREATE INDEX `index_private_group_item_group_id_expires_at` ON `$tableName` (`group_id`, `expires_at`)');
   }
 
-  Future<PrivateGroupItemSchema?> insert(PrivateGroupItemSchema schema) async {
+  Future<PrivateGroupItemSchema?> insert(PrivateGroupItemSchema? schema) async {
+    if (db?.isOpen != true) return null;
+    if (schema == null || schema.groupId.isEmpty) return null;
     Map<String, dynamic> entity = schema.toMap();
-    try {
-      int? id = await db?.insert(tableName, entity);
+    return await _queue.add(() async {
+      try {
+        int? id = await db?.transaction((txn) {
+          return txn.insert(tableName, entity);
+        });
+        if (id != null) {
+          PrivateGroupItemSchema schema = PrivateGroupItemSchema.fromMap(entity);
+          schema.id = id;
+          logger.v("$TAG - insert - success - schema:$schema");
+          return schema;
+        } else {
+          logger.i("$TAG - insert - fail - schema:$schema");
+        }
+      } catch (e, st) {
+        handleError(e, st);
+      }
+      return null;
+    });
+  }
 
-      if (id != null && id > 0) {
-        schema.id = id;
-        logger.v("$TAG - insert - success - schema:$schema");
+  Future<PrivateGroupItemSchema?> queryByInvitee(String? groupId, String? invitee) async {
+    if (db?.isOpen != true) return null;
+    if (groupId == null || groupId.isEmpty || invitee == null || invitee.isEmpty) return null;
+    try {
+      List<Map<String, dynamic>>? res = await db?.query(
+        tableName,
+        columns: ['*'],
+        where: 'group_id = ? AND invitee = ?',
+        whereArgs: [groupId, invitee],
+        offset: 0,
+        limit: 1,
+        orderBy: 'expires_at DESC',
+      );
+      if (res != null && res.length > 0) {
+        PrivateGroupItemSchema? schema = PrivateGroupItemSchema.fromMap(res.first);
+        logger.v("$TAG - queryByInvitee - success - invitee:$invitee - schema:$schema");
         return schema;
       }
-      logger.w("$TAG - insert - empty - schema:$schema");
     } catch (e, st) {
       handleError(e, st);
     }
     return null;
   }
 
-  Future<List<PrivateGroupItemSchema>?> query(String groupId) async {
-    try {
-      List<Map<String, dynamic>>? res = await db?.transaction((txn) {
-        return txn.query(
-          tableName,
-          columns: ['*'],
-          where: 'group_id = ?',
-          whereArgs: [groupId],
-        );
-      });
-      if (res != null && res.length > 0) {
-        List<PrivateGroupItemSchema> list = [];
-        for (var i = 0; i < res.length; i++) {
-          if (res[i].isEmpty || res[i].isEmpty) continue;
-          Map<String, dynamic> map = res[i];
-          PrivateGroupItemSchema schema = PrivateGroupItemSchema.fromMap(map);
-          list.add(schema);
-        }
-        logger.v("$TAG - query - success");
-        return list;
-      }
-      logger.v("$TAG - query - empty - groupId:$groupId");
-    } catch (e, st) {
-      handleError(e, st);
-    }
-    return null;
-  }
-
-  Future<List<PrivateGroupItemSchema>?> queryLimit(String groupId, {String? orderBy, int offset = 0, int limit = 20}) async {
+  Future<List<PrivateGroupItemSchema>> queryList(String? groupId, {int offset = 0, int limit = 20}) async {
+    if (db?.isOpen != true) return [];
+    if (groupId == null || groupId.isEmpty) return [];
     try {
       List<Map<String, dynamic>>? res = await db?.transaction((txn) {
         return txn.query(
@@ -98,63 +99,25 @@ class PrivateGroupItemStorage with Tag {
           whereArgs: [groupId],
           offset: offset,
           limit: limit,
-          orderBy: orderBy ?? 'id ASC',
+          orderBy: 'expires_at DESC',
         );
       });
-      if (res != null && res.length > 0) {
-        List<PrivateGroupItemSchema> list = [];
-        for (var i = 0; i < res.length; i++) {
-          if (res[i].isEmpty || res[i].isEmpty) continue;
-          Map<String, dynamic> map = res[i];
-          PrivateGroupItemSchema schema = PrivateGroupItemSchema.fromMap(map);
-          list.add(schema);
-        }
-        logger.v("$TAG - query - success");
-        return list;
+      if (res == null || res.isEmpty) {
+        logger.v("$TAG - queryList - empty - groupId:$groupId");
+        return [];
       }
-      logger.v("$TAG - query - empty - groupId:$groupId");
-    } catch (e, st) {
-      handleError(e, st);
-    }
-    return null;
-  }
-
-  Future<PrivateGroupItemSchema?> queryByInvitee(String groupId, String invitee) async {
-    try {
-      List<Map<String, dynamic>>? res = await db?.query(
-        tableName,
-        columns: ['*'],
-        where: 'group_id = ? AND invitee = ?',
-        whereArgs: [groupId, invitee],
-      );
-      if (res != null && res.length > 0) {
-        PrivateGroupItemSchema? schema = PrivateGroupItemSchema.fromMap(res.first);
-        logger.v("$TAG - query - success - invitee:$invitee - schema:$schema");
-        return schema;
-      }
-    } catch (e, st) {
-      handleError(e, st);
-    }
-    return null;
-  }
-
-  Future<int?> delete() async {
-    try {
-      int? res = await db?.transaction((txn) {
-        return txn.delete(tableName);
+      List<PrivateGroupItemSchema> results = <PrivateGroupItemSchema>[];
+      String logText = '';
+      res.forEach((map) {
+        logText += "\n      $map";
+        PrivateGroupItemSchema pgItem = PrivateGroupItemSchema.fromMap(map);
+        results.add(pgItem);
       });
-      return res;
+      logger.v("$TAG - queryList - items:$logText");
+      return results;
     } catch (e, st) {
       handleError(e, st);
     }
-    return null;
-  }
-
-  Future<void> dropTable() async {
-    try {
-      await db?.execute('DROP TABLE $tableName');
-    } catch (e, st) {
-      handleError(e, st);
-    }
+    return [];
   }
 }
