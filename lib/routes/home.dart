231ae982c4661694f37bf0fe0350655402407ea8
guo <jiangzhiguo1992@163.com>
2022-10-24 18:49:15 +0800

    fixed ios video touch

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nmobile/routes/routes.dart';
+import 'package:nmobile/screens/common/media.dart';
 import 'package:nmobile/screens/common/photo.dart';
 import 'package:nmobile/screens/common/scanner.dart';
 import 'package:nmobile/screens/common/select.dart';
@@ -8,8 +9,9 @@ import 'package:nmobile/screens/common/video.dart';
 Map<String, WidgetBuilder> _routes = {
   ScannerScreen.routeName: (BuildContext context) => ScannerScreen(),
   SelectScreen.routeName: (BuildContext context, {arguments}) => SelectScreen(arguments: arguments),
-  PhotoScreen.routeName: (BuildContext context, {arguments}) => PhotoScreen(arguments: arguments),
-  VideoScreen.routeName: (BuildContext context, {arguments}) => VideoScreen(arguments: arguments),
+  MediaScreen.routeName: (BuildContext context, {arguments}) => MediaScreen(arguments: arguments),
+  // PhotoScreen.routeName: (BuildContext context, {arguments}) => PhotoScreen(arguments: arguments),
+  // VideoScreen.routeName: (BuildContext context, {arguments}) => VideoScreen(arguments: arguments),
 };
 
 init() {
