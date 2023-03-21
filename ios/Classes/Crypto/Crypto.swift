ab5cb4e3a9b05195345fe602985f157484b68430
guo <jiangzhiguo1992@163.com>
2023-03-21 12:09:12 +0800

    format ios native code

--- ios/Classes/Crypto/Crypto.swift
+++ ios/Classes/Crypto/Crypto.swift
@@ -8,11 +8,11 @@
 import Nkn
 
 class Crypto : ChannelBase, FlutterStreamHandler {
-
+    
     static var instance: Crypto = Crypto()
     let qryptoQueue = DispatchQueue(label: "org.nkn.mobile/native/crypto/queue", qos: .default, attributes: .concurrent)
     private var qryptoWorkItem: DispatchWorkItem?
-
+    
     var methodChannel: FlutterMethodChannel?
     let METHOD_CHANNEL_NAME = "org.nkn.mobile/native/crypto_method"
     var eventSink: FlutterEventSink?
@@ -20,26 +20,26 @@ class Crypto : ChannelBase, FlutterStreamHandler {
     public static func register(controller: FlutterViewController) {
         instance.install(binaryMessenger: controller as! FlutterBinaryMessenger)
     }
-
+    
     func install(binaryMessenger: FlutterBinaryMessenger) {
         self.methodChannel = FlutterMethodChannel(name: METHOD_CHANNEL_NAME, binaryMessenger: binaryMessenger)
         self.methodChannel?.setMethodCallHandler(handle)
     }
-
+    
     func uninstall() {
         self.methodChannel?.setMethodCallHandler(nil)
     }
-
+    
     func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
         eventSink = events
         return nil
     }
-
+    
     func onCancel(withArguments arguments: Any?) -> FlutterError? {
         // eventSink = nil
         return nil
     }
-
+    
     private func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method{
         case "gcmEncrypt":
@@ -50,17 +50,17 @@ class Crypto : ChannelBase, FlutterStreamHandler {
             result(FlutterMethodNotImplemented)
         }
     }
-
+    
     private func create(_ call: FlutterMethodCall, result: FlutterResult) {
         result(nil)
     }
-
+    
     private func gcmEncrypt(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let args = call.arguments as! [String: Any]
         let data = args["data"] as? FlutterStandardTypedData
         let key = args["key"] as? FlutterStandardTypedData
         let nonceSize = args["nonceSize"] as? Int ?? 0
-
+        
         qryptoWorkItem = DispatchWorkItem {
             var error: NSError?
             let cipherText = CryptoGCMEncrypt(data?.data, key?.data, nonceSize, &error)
@@ -79,7 +79,7 @@ class Crypto : ChannelBase, FlutterStreamHandler {
         let data = args["data"] as? FlutterStandardTypedData
         let key = args["key"] as? FlutterStandardTypedData
         let nonceSize = args["nonceSize"] as? Int ?? 0
-
+        
         qryptoWorkItem = DispatchWorkItem {
             var error: NSError?
             let plainText = CryptoGCMDecrypt(data?.data, key?.data, nonceSize, &error)
