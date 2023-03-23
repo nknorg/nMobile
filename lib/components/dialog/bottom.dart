4f4ba9e9d1e38d3b1141ec440a3655f674b6c5b5
guo <jiangzhiguo1992@163.com>
2023-03-23 17:02:50 +0800

    update badge

--- lib/components/dialog/bottom.dart
+++ lib/components/dialog/bottom.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nmobile/blocs/wallet/wallet_bloc.dart';
 import 'package:nmobile/blocs/wallet/wallet_state.dart';
-import 'package:nmobile/common/client/client.dart';
 import 'package:nmobile/common/locator.dart';
 import 'package:nmobile/common/settings.dart';
 import 'package:nmobile/components/base/stateful.dart';
