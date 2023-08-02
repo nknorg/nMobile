2c77ad7fc2506eb577ede48dab1e85254e8468f1
guo <jiangzhiguo1992@163.com>
2023-08-02 15:37:54 +0800

    remove resolvers dep from nkn-go-sdk

--- golib/gomobile.go
+++ golib/gomobile.go
@@ -1,8 +1,6 @@
 package nkngolib
 
 import (
-	dnsresolver "github.com/nknorg/dns-resolver-go"
-	ethresolver "github.com/nknorg/eth-resolver-go"
 	"github.com/nknorg/nkn-sdk-go"
 	"github.com/nknorg/nkngomobile"
 	"github.com/nknorg/reedsolomon"
@@ -11,8 +9,6 @@ import (
 
 var (
 	_ = nkn.NewStringArray
-	_ = dnsresolver.NewResolver
-	_ = ethresolver.NewResolver
 	_ = nkngomobile.NewStringArray
 	_ = reedsolomon.New
 	_ = bind.GenGo
