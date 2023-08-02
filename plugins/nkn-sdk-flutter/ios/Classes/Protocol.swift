a7b65db000dd7fa89ea2edb718f44f8ff56490a5
guo <jiangzhiguo1992@163.com>
2023-08-02 17:08:01 +0800

    remove resolvers dep from ios_native

--- plugins/nkn-sdk-flutter/ios/Classes/Protocol.swift
+++ plugins/nkn-sdk-flutter/ios/Classes/Protocol.swift
@@ -8,24 +8,24 @@ class MapProtocol : NSObject, NkngomobileStringMapFuncProtocol {
     }
 }
 
-class EthResolver : NSObject, NkngomobileResolverProtocol{
-    let resolver: EthresolverResolver?
-    init(config: EthresolverConfig?) {
-        var error: NSError?
-        self.resolver = EthresolverNewResolver(config, &error)
-    }
-    func resolve(_ address: String?, error: NSErrorPointer) -> String {
-        return self.resolver!.resolve(address, error: error)
-    }
-}
-
-class DnsResolver : NSObject, NkngomobileResolverProtocol{
-    let resolver: DnsresolverResolver?
-    init(config: DnsresolverConfig?) {
-        var error: NSError?
-        self.resolver = DnsresolverNewResolver(config, &error)
-    }
-    func resolve(_ address: String?, error: NSErrorPointer) -> String {
-        return self.resolver!.resolve(address, error: error)
-    }
-}
+//class EthResolver : NSObject, NkngomobileResolverProtocol{
+//    let resolver: EthresolverResolver?
+//    init(config: EthresolverConfig?) {
+//        var error: NSError?
+//        self.resolver = EthresolverNewResolver(config, &error)
+//    }
+//    func resolve(_ address: String?, error: NSErrorPointer) -> String {
+//        return self.resolver!.resolve(address, error: error)
+//    }
+//}
+//
+//class DnsResolver : NSObject, NkngomobileResolverProtocol{
+//    let resolver: DnsresolverResolver?
+//    init(config: DnsresolverConfig?) {
+//        var error: NSError?
+//        self.resolver = DnsresolverNewResolver(config, &error)
+//    }
+//    func resolve(_ address: String?, error: NSErrorPointer) -> String {
+//        return self.resolver!.resolve(address, error: error)
+//    }
+//}
