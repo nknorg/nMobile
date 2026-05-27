d69f8a3729c9793feeeb4ab2ebe23434469c746e
Heron <peng.liu@nkn.org>
2026-05-27 16:21:56 +0800

    Update android project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 android {
     namespace = "org.nkn.mobile.app"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
